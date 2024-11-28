0f5192766114023aed7326293ddb1319bb6b7110
wynter24 <m.wynter24.k@gmail.com>
2024-11-28 16:49:29 +0900

    ♻️[Refactor] Home 페이지 button 컴포넌트에 props 추가

--- src/app/bookclub/page.tsx
+++ src/app/bookclub/page.tsx
@@ -3,7 +3,7 @@ import Button from '@/components/button/Button';
 export default function Home() {
   return (
     <div>
-      <Button />
+      <Button size="large" state="active" text="생성하기" />
     </div>
   );
 }
