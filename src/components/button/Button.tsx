50222e6f68e4e3c958c5594d239a4459163ec499
wynter24 <m.wynter24.k@gmail.com>
2024-11-28 15:14:34 +0900

    ✨[Feat] 버튼 컴포넌트 생성 및 초기 디자인 설정

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,7 +1,58 @@
 import React from 'react';
+import { twMerge } from 'tailwind-merge';
 
-const Button = () => {
-  return <div>Button</div>;
+type ButtonProps = {
+  text: string;
+  size: 'large' | 'small';
+  state: 'active' | 'disabled' | 'none';
+  borderColor?: string; // 사용자 지정
+  textColor?: string; // 사용자 지정
+} & React.ComponentPropsWithoutRef<'button'>;
+
+const Button = ({
+  text,
+  size,
+  state,
+  borderColor,
+  textColor,
+  ...buttonProps
+}: ButtonProps) => {
+  let sizeClasses = '';
+  if (size === 'large') {
+    sizeClasses = 'w-[332px] h-[44px] text-base';
+  } else if (size === 'small') {
+    sizeClasses = 'w-[120px] h-[40px] text-sm';
+  }
+
+  let stateClasses = '';
+  switch (state) {
+    case 'active':
+      stateClasses = 'bg-orange-600 text-white';
+      break;
+    case 'disabled':
+      stateClasses = 'bg-gray-400 text-white cursor-not-allowed';
+      break;
+    case 'none':
+    default:
+      stateClasses = 'bg-white text-orange-600 border border-orange-600';
+      break;
+  }
+
+  const buttonClassName = twMerge(
+    `${sizeClasses} ${stateClasses} rounded-[12px] font-semibold`,
+  );
+
+  // state가 none일 때, 사용자가 스타일 지정
+  const inlineStyles = {
+    borderColor: borderColor ? borderColor : undefined,
+    color: textColor ? textColor : undefined,
+  };
+
+  return (
+    <button {...buttonProps} className={buttonClassName} style={inlineStyles}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
