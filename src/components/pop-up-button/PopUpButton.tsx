a324e8590f85b0a99838d9717aad22204f1275b6
sunnwave <ssun20886@gmail.com>
2024-11-29 00:33:05 +0900

    ✨[Feat] PopUp의 Button 컴포넌트 구현, 스토리북 코드 작성 #31

---
+++ src/components/pop-up-button/PopUpButton.tsx
@@ -0,0 +1,20 @@
+const THEME_COLOR = {
+  cancel: 'border border-orange-600 bg-white text-orange-600 mr-[8px]',
+  confirm: 'bg-orange-600 text-white',
+};
+
+interface PopUpButtonProps {
+  isConfirm: boolean;
+}
+
+function PopUpButton({ isConfirm }: PopUpButtonProps) {
+  return (
+    <button
+      className={`h-[44px] w-[120px] rounded-lg ${isConfirm ? THEME_COLOR.confirm : THEME_COLOR.cancel}`}
+    >
+      {isConfirm ? '확인' : '취소'}
+    </button>
+  );
+}
+
+export default PopUpButton;
--- src/components/pop-up/PopUp.tsx
+++ src/components/pop-up/PopUp.tsx
@@ -1,15 +1,19 @@
-function PopUp() {
+import PopUpButton from '../pop-up-button/PopUpButton';
+
+interface PopUpProps {
+  isLarge: boolean;
+  isSingleButton: boolean;
+  label: string;
+}
+
+function PopUp({ label }: PopUpProps) {
   return (
     <div className="flex h-[212px] w-[450px] flex-col items-center justify-between rounded-lg border border-gray-900 bg-white p-[24px]">
       <button className="ml-auto">닫기</button>
-      <div className="flex flex-col items-center">가입이 완료되었습니다!</div>
+      <div className="flex flex-col items-center">{label}</div>
       <div className="flex items-center">
-        <button className="mr-[8px] h-[44px] w-[120px] rounded-lg border border-orange-600 bg-white text-orange-600">
-          취소
-        </button>
-        <button className="h-[44px] w-[120px] rounded-lg bg-orange-600 text-white">
-          확인
-        </button>
+        <PopUpButton isConfirm={false} />
+        <PopUpButton isConfirm={true} />
       </div>
     </div>
   );
