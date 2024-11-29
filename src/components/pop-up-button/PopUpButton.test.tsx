ece378076ba40147c260c556455f4f9e818cd920
sunnwave <ssun20886@gmail.com>
2024-11-29 13:51:25 +0900

    💬[Comment] 테스트코드 이슈번호 추가 #32

--- src/components/pop-up-button/PopUpButton.test.tsx
+++ src/components/pop-up-button/PopUpButton.test.tsx
@@ -8,17 +8,17 @@ describe('PopUp Button 렌더링 테스트', () => {
     const popUpButton = screen.getByRole('button');
     expect(popUpButton.textContent).toBe('확인');
   });
-  it('isConfirm이 true일 때 취소 버튼 렌더링 실패 테스트', () => {
-    render(<PopUpButton isConfirm={true} />);
-    const popUpButton = screen.getByRole('button');
-    expect(popUpButton.textContent).not.toBe('취소');
-  });
   it('isConfirm이 false일 때 취소 버튼 렌더링', () => {
     render(<PopUpButton isConfirm={false} />);
     const popUpButton = screen.getByRole('button');
     expect(popUpButton.textContent).toBe('취소');
   });
 
+  it('isConfirm이 true일 때 취소 버튼 렌더링 실패 테스트', () => {
+    render(<PopUpButton isConfirm={true} />);
+    const popUpButton = screen.getByRole('button');
+    expect(popUpButton.textContent).not.toBe('취소');
+  });
   it('isConfirm이 false일 때 확인 버튼 렌더링 실패 테스트', () => {
     render(<PopUpButton isConfirm={false} />);
     const popUpButton = screen.getByRole('button');
