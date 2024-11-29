616373102220d23441fc311321cbb3e6874b9227
cloud0406 <cloud0406@naver.com>
2024-11-29 18:04:16 +0900

    ✨[Feat] 마감 지우기 아이콘 개발

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as IcCheck } from './IcCheck';
 export { default as HeartIcon } from './HeartIcon';
 export { default as RightArrow } from './RightArrow';
+export { default as WaveIcon } from './WaveIcon';
