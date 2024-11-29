75a86879533479a5a4f6d876f7d7e6193f2eb5e5
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-11-29 13:30:41 +0900

    [Feat] 비밀번호 가리기 / 보기 기능 구현 #34

--- src/features/auth/components/FormField.tsx
+++ src/features/auth/components/FormField.tsx
@@ -1,3 +1,9 @@
+'use client';
+
+import { useState } from 'react';
+import VisibilityOn from '../../../../public/icons/visibility_on';
+import VisibilityOff from '../../../../public/icons/visibility_off';
+
 interface FormFieldProps {
   label: string;
   type: string;
@@ -6,18 +12,31 @@ interface FormFieldProps {
 }
 
 function FormField({ label, type, placeholder, id }: FormFieldProps) {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className="flex w-full flex-col gap-2">
       <label htmlFor={id} className="font-semibold">
         {label}
       </label>
-      <div className="w-full">
+      <div className="relative w-full">
         <input
           id={id}
-          type={type}
+          type={
+            type === 'password' ? (showPassword ? 'text' : 'password') : type
+          }
           placeholder={placeholder}
           className="w-full rounded-lg bg-gray-50 p-2 sm:p-3"
         />
+        {type === 'password' && (
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-3 top-1/2 -translate-y-1/2"
+          >
+            {showPassword ? <VisibilityOn /> : <VisibilityOff />}
+          </button>
+        )}
       </div>
     </div>
   );
