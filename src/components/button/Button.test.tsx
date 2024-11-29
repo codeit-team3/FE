814b117f6d49177bad65e50b2234bb0e6ed6b6ed
wynter24 <m.wynter24.k@gmail.com>
2024-11-29 14:20:15 +0900

    ✅[Test] 스타일 관련 테스트 코드 삭제 #11

--- src/components/button/Button.test.tsx
+++ src/components/button/Button.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import Button from './Button';
 
 describe('Button', () => {
   it('버튼이 정상적으로 렌더링', () => {
-    render(<Button size="small" state="active" text="생성하기" />);
+    render(<Button size="small" hasBackground={true} text="생성하기" />);
     const buttonElement = screen.getByText('생성하기');
     expect(buttonElement).toBeInTheDocument();
   });
@@ -18,7 +18,7 @@ describe('Button', () => {
       <Button
         onClick={handleClick}
         size="small"
-        state="active"
+        hasBackground={true}
         text="제출하기"
       />,
     );
@@ -29,63 +29,5 @@ describe('Button', () => {
 
     // Then: 콜백 함수가 호출되었는지 검증
     expect(handleClick).toHaveBeenCalledTimes(1);
-  });
-
-  test('버튼이 disabled 상태일 때 클릭 이벤트가 발생되지 않는다', () => {
-    render(
-      <Button disabled size="small" state="disabled" text="비활성화 버튼" />,
-    );
-    const buttonElement = screen.getByText('비활성화 버튼');
-    expect(buttonElement).toBeDisabled();
-
-    fireEvent.click(buttonElement);
-    expect(buttonElement).toHaveClass(
-      'bg-gray-400 text-white cursor-not-allowed',
-    );
-    expect(buttonElement).toBeDisabled();
-  });
-
-  // 스타일 테스트
-  it('버튼의 크기와 상태에 따른 스타일 클래스가 올바르게 적용되는지 확인', () => {
-    // Large Size Test
-    render(<Button size="large" state="none" text="큰 버튼" />);
-    const largeButton = screen.getByText('큰 버튼');
-    expect(largeButton).toHaveClass('w-[332px] h-[44px] text-base');
-
-    // Small Size Test
-    render(<Button size="small" state="none" text="작은 버튼" />);
-    const smallButton = screen.getByText('작은 버튼');
-    expect(smallButton).toHaveClass('w-[120px] h-[40px] text-sm');
-  });
-
-  it('버튼의 상태에 따른 스타일이 올바르게 적용되는지 확인', () => {
-    // Active State Test
-    render(<Button size="small" state="active" text="활성화된 버튼" />);
-    const activeButton = screen.getByText('활성화된 버튼');
-    expect(activeButton).toHaveClass('bg-orange-600 text-white');
-
-    // None State Test
-    render(<Button size="small" state="none" text="기본 버튼" />);
-    const noneButton = screen.getByText('기본 버튼');
-    expect(noneButton).toHaveClass(
-      'bg-white text-orange-600 border border-orange-600',
-    );
-  });
-
-  it('사용자 지정 스타일이 인라인 스타일로 올바르게 적용되는지 확인', () => {
-    render(
-      <Button
-        size="large"
-        state="none"
-        text="사용자 정의 버튼"
-        borderColor="red"
-        textColor="blue"
-      />,
-    );
-    const buttonElement = screen.getByText('사용자 정의 버튼');
-    expect(buttonElement).toHaveStyle({
-      borderColor: 'red',
-      color: 'blue',
-    });
   });
 });
