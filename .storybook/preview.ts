f5173d607dcf54b96629e9488b308c5a9086eefc
cloud0406 <cloud0406@naver.com>
2024-11-29 15:16:32 +0900

    ✅ [Test] 스토리북 및 preview 추가 #20

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -9,6 +9,31 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    viewport: {
+      viewports: {
+        mobile: {
+          name: 'Mobile',
+          styles: {
+            width: '375px',
+            height: '1232px',
+          },
+        },
+        tablet: {
+          name: 'Tablet',
+          styles: {
+            width: '744px',
+            height: '1133px',
+          },
+        },
+        desktop: {
+          name: 'Desktop',
+          styles: {
+            width: '1920px',
+            height: '1080px',
+          },
+        },
+      },
+    },
   },
 };
 
