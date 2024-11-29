319031e6663ac2bdb85bad0be03a62fdc5ac1a31
wynter24 <m.wynter24.k@gmail.com>
2024-11-29 11:05:12 +0900

    📦[Chore] tailwind content 수정 #11

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/*/.{html,js}',
-  ],
+  content: ['./src/*/.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
