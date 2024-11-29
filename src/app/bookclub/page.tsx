744010031d1d2b51ced18491e2e0e1b028adda35
wynter24 <m.wynter24.k@gmail.com>
2024-11-29 14:08:55 +0900

    ♻️[Refactor] button props 변경 #11

--- src/app/bookclub/page.tsx
+++ src/app/bookclub/page.tsx
@@ -3,7 +3,13 @@ import Button from '@/components/button/Button';
 export default function Home() {
   return (
     <div>
-      <Button size="large" state="active" text="생성하기" />
+      <Button
+        size="large"
+        hasBackground={false}
+        text="생성하기"
+        outlineColor="blue"
+        fontColor="blue"
+      />
     </div>
   );
 }
