0a71ff7b302928252ffc795f52ae8da9d976f1a7
wynter24 <m.wynter24.k@gmail.com>
2024-11-29 14:05:43 +0900

    ♻️[Refactor] button props 변경 및 해당 스타일 설정 #11

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,19 +4,23 @@ import { twMerge } from 'tailwind-merge';
 interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
   text: string;
   size: 'large' | 'small';
-  state: 'active' | 'disabled' | 'none';
-  borderColor?: string; // 사용자 지정
-  textColor?: string; // 사용자 지정
+  hasBackground: boolean;
+  backgroundColor?: string;
+  outlineColor?: string;
+  fontColor?: string;
 }
 
 const Button = ({
   text,
   size,
-  state,
-  borderColor,
-  textColor,
+  hasBackground,
+  backgroundColor,
+  outlineColor,
+  fontColor,
   ...buttonProps
 }: ButtonProps) => {
+  const { disabled } = buttonProps;
+
   let sizeClasses = '';
   if (size === 'large') {
     sizeClasses = 'min-w-[332px] h-[44px] px-4 text-base';
@@ -24,29 +28,26 @@ const Button = ({
     sizeClasses = 'min-w-[120px] h-[40px] px-3 text-sm';
   }
 
-  let stateClasses = '';
-  switch (state) {
-    case 'active':
-      stateClasses = 'bg-orange-600 text-white';
-      break;
-    case 'disabled':
-      stateClasses = 'bg-gray-400 text-white';
-      break;
-    case 'none':
-    default:
-      stateClasses = 'bg-white text-orange-600 border border-orange-600';
-      break;
+  const baseClasses = 'rounded-[12px] font-semibold';
+
+  let bgClasses = '';
+
+  if (hasBackground) {
+    bgClasses = disabled
+      ? `bg-gray-400 text-white cursor-not-allowed`
+      : `bg-orange-600 text-white cursor-pointer`;
+  } else {
+    bgClasses = disabled
+      ? 'bg-white text-gray-400 border border-gray-400 cursor-not-allowed'
+      : 'bg-white text-orange-600 border border-orange-600 cursor-pointer';
   }
 
-  const buttonClassName = twMerge(
-    `${sizeClasses} ${stateClasses} rounded-[12px] font-semibold cursor-pointer`,
-    state === 'disabled' && 'cursor-not-allowed',
-  );
+  const buttonClassName = twMerge(sizeClasses, baseClasses, bgClasses);
 
-  // state가 none일 때, 사용자가 스타일 지정
-  const inlineStyles = {
-    borderColor: borderColor ? borderColor : undefined,
-    color: textColor ? textColor : undefined,
+  const inlineStyles: React.CSSProperties = {
+    backgroundColor: hasBackground && !disabled ? backgroundColor : undefined,
+    borderColor: !hasBackground && !disabled ? outlineColor : undefined,
+    color: !hasBackground && !disabled ? fontColor : undefined,
   };
 
   return (
