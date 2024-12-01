bcfec4e46778f9fed9555860caef6fc259bdbe53
sunnwave <ssun20886@gmail.com>
2024-12-01 18:08:31 +0900

    ✅[Test] 팝업창 스토리북 테스트코드 타입별 코드 추가 #26

--- src/components/pop-up/PopUp.stories.tsx
+++ src/components/pop-up/PopUp.stories.tsx
@@ -1,6 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import PopUp from './PopUp';
 
+const POPUP_LABEL = {
+  large: `정말 나가시겠어요?\n작성된 내용이 모두 삭제됩니다.`,
+  small: `가입이 완료되었습니다!`,
+};
+
 const meta: Meta<typeof PopUp> = {
   title: 'components/PopUp',
   component: PopUp,
@@ -10,9 +15,39 @@ const meta: Meta<typeof PopUp> = {
 };
 
 type Story = StoryObj<typeof PopUp>;
-export const Primary: Story = {
+export const LargeOneButton: Story = {
   args: {
-    // 팝업 props
+    isOpen: true,
+    isLarge: true,
+    isTwoButton: false,
+    label: POPUP_LABEL.large,
+  },
+};
+
+export const LargeTwoButton: Story = {
+  args: {
+    isOpen: true,
+    isLarge: true,
+    isTwoButton: true,
+    label: POPUP_LABEL.large,
+  },
+};
+
+export const SmallOneButton: Story = {
+  args: {
+    isOpen: true,
+    isLarge: false,
+    isTwoButton: false,
+    label: POPUP_LABEL.small,
+  },
+};
+
+export const SmallTwoButton: Story = {
+  args: {
+    isOpen: true,
+    isLarge: false,
+    isTwoButton: true,
+    label: POPUP_LABEL.small,
   },
 };
 
