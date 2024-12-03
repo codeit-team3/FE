43b4c92e72f5c72fd455a08d0c6e5dc890b1df20
cloud0406 <cloud0406@naver.com>
2024-12-03 18:32:30 +0900

    💄[Design] 임시 레이아웃 및 이미지 섹션 디자인 구현

--- src/app/bookclub/[id]/page.tsx
+++ src/app/bookclub/[id]/page.tsx
@@ -1,14 +1,42 @@
-interface BookClubDetailPageProps {
-  params: {
-    id: string;
-  };
-}
+import BorderedBox from '@/components/bordered-box/BorderedBox';
+import Image from 'next/image';
 
-function BookClubDetailPage({ params }: BookClubDetailPageProps) {
+function BookClubDetailPage({}) {
   return (
-    <div>
-      <h1>북클럽 상세 페이지</h1>
-      <p>북클럽 ID: {params.id}</p>
+    <div className="flex w-full flex-col gap-4 p-4">
+      {/* 이미지 영역 */}
+      <BorderedBox nonePadding>
+        <div className="relative h-[180px] w-full">
+          <Image
+            src="https://picsum.photos/200/300?random=1"
+            alt="북클럽 이미지"
+            fill
+            className="rounded-3xl object-cover"
+          />
+        </div>
+      </BorderedBox>
+
+      {/* 모임 정보 */}
+      <div className="flex flex-col bg-white p-4 shadow-md">
+        <div>모임 정보 영역</div>
+      </div>
+
+      {/* 모임 상세 설명 */}
+      <div className="flex flex-col bg-white p-4 shadow-md">
+        <div>모임 상세 설명 영역</div>
+      </div>
+
+      {/* 사용자 리뷰 */}
+      <div className="flex flex-col bg-white p-4 shadow-md">
+        <div>사용자 리뷰 영역</div>
+      </div>
+
+      {/* 참여하기 버튼 */}
+      <div className="flex justify-center bg-white p-4 shadow-md">
+        <button className="rounded bg-orange-500 px-4 py-2 text-white">
+          참여하기
+        </button>
+      </div>
     </div>
   );
 }
