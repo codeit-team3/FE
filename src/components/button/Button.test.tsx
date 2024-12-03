3cbedeeae61bf51323839b382dccb4c8f901472c
wynter24 <m.wynter24.k@gmail.com>
2024-12-03 10:30:40 +0900

    💬[Comment] test 코드 주석 정리 #11

--- src/components/button/Button.test.tsx
+++ src/components/button/Button.test.tsx
@@ -11,9 +11,8 @@ describe('Button', () => {
   });
 
   it('버튼 클릭시 이벤트 핸들러 호출', async () => {
-    // Given: 사용자와 화면이 준비되어 있고, 화면에는 콜백이 연결된 버튼이 존재
-    const user = userEvent.setup(); // 사용자 이벤트 설정
-    const handleClick = jest.fn(); // 콜백 함수를 모킹
+    const user = userEvent.setup();
+    const handleClick = jest.fn();
     render(
       <Button
         onClick={handleClick}
@@ -23,11 +22,9 @@ describe('Button', () => {
       />,
     );
 
-    // When: 사용자가 '클릭하세요'라는 버튼을 클릭
     const buttonElement = screen.getByRole('button', { name: '제출하기' });
     await user.click(buttonElement);
 
-    // Then: 콜백 함수가 호출되었는지 검증
     expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
