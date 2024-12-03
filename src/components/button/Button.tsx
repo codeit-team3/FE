383eb76c2e7b07806801e9f9ac6f2a3609c9c71c
wynter24 <m.wynter24.k@gmail.com>
2024-12-03 10:52:05 +0900

    ♻️[Refactor] 사이즈, 배경, 색상을 객체 리터럴로 변경 및 variant 추가 #11

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,17 +3,42 @@ import { twMerge } from 'tailwind-merge';
 
 interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
   text: string;
-  size: 'large' | 'small';
+  size: 'large' | 'small' | 'modal';
   hasBackground: boolean;
+  variantColor?: 'default';
   backgroundColor?: string;
   outlineColor?: string;
   fontColor?: string;
 }
 
+const sizeVariants = {
+  modal: 'min-w-[120px] h-[44px] px-4 text-base',
+  large: 'min-w-[332px] h-[44px] px-4 text-base',
+  small: 'min-w-[120px] h-[40px] px-3 text-sm',
+};
+
+const backgroundVariants = {
+  enabled: {
+    filled: {
+      default: 'bg-orange-600 text-white cursor-pointer',
+      // 색상 추가
+    },
+    outline: {
+      default:
+        'bg-white text-orange-600 border border-orange-600 cursor-pointer',
+    },
+  },
+  disabled: {
+    filled: 'bg-gray-400 text-white cursor-not-allowed',
+    outline: 'bg-white text-gray-400 border border-gray-400 cursor-not-allowed',
+  },
+};
+
 const Button = ({
   text,
   size,
   hasBackground,
+  variantColor = 'default',
   backgroundColor,
   outlineColor,
   fontColor,
@@ -21,26 +46,12 @@ const Button = ({
 }: ButtonProps) => {
   const { disabled } = buttonProps;
 
-  let sizeClasses = '';
-  if (size === 'large') {
-    sizeClasses = 'min-w-[332px] h-[44px] px-4 text-base';
-  } else if (size === 'small') {
-    sizeClasses = 'min-w-[120px] h-[40px] px-3 text-sm';
-  }
-
+  const sizeClasses = sizeVariants[size];
   const baseClasses = 'rounded-[12px] font-semibold';
-
-  let bgClasses = '';
-
-  if (hasBackground) {
-    bgClasses = disabled
-      ? `bg-gray-400 text-white cursor-not-allowed`
-      : `bg-orange-600 text-white cursor-pointer`;
-  } else {
-    bgClasses = disabled
-      ? 'bg-white text-gray-400 border border-gray-400 cursor-not-allowed'
-      : 'bg-white text-orange-600 border border-orange-600 cursor-pointer';
-  }
+  const styleType = hasBackground ? 'filled' : 'outline';
+  const bgClasses = disabled
+    ? backgroundVariants['disabled'][styleType]
+    : backgroundVariants['enabled'][styleType][variantColor];
 
   const buttonClassName = twMerge(sizeClasses, baseClasses, bgClasses);
 
