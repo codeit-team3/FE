2dde641dd25ba049e3738e1baab118e131b4333c
wynter24 <m.wynter24.k@gmail.com>
2024-12-03 16:43:23 +0900

    ♻️[Refactor]  배경스타일 및 색상 variant 변경 #11

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,45 +4,43 @@ import { twMerge } from 'tailwind-merge';
 interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
   text: string;
   size: 'large' | 'small' | 'modal';
-  hasBackground: boolean;
-  variantColor?: 'default' | 'gray';
+  fillType: 'solid' | 'bordered';
+  themeColor: 'orange-600' | 'gray-400';
 }
 
-const SIZE_VARIANTS = {
+const SIZE = {
   modal: 'min-w-[120px] h-[44px] px-4 text-base',
   large: 'min-w-[332px] h-[44px] px-4 text-base',
   small: 'min-w-[120px] h-[40px] px-3 text-sm',
 };
 
-const BACKGROUND_VARIANTS = {
-  filled: {
-    default: 'bg-orange-600 text-white cursor-pointer',
-    gray: 'bg-gray-400 text-white cursor-pointer',
-  },
-  outline: {
-    default: 'bg-white text-orange-600 border border-orange-600 cursor-pointer',
-    gray: 'bg-white text-gray-400 border border-gray-400 cursor-pointer',
-  },
+const COLOR_STYLES = {
+  'orange-600': ['bg-orange-600', 'text-orange-600', 'border-orange-600'],
+  'gray-400': ['bg-gray-400', 'text-gray-400', 'border-gray-400'],
 };
 
 export default function Button({
   text,
   size,
-  hasBackground,
-  variantColor = 'default',
+  fillType,
+  themeColor,
   ...buttonProps
 }: ButtonProps) {
   const { disabled } = buttonProps;
 
-  const sizeClasses = SIZE_VARIANTS[size];
-  const baseClasses = 'rounded-[12px] font-semibold';
-  const styleType = hasBackground ? 'filled' : 'outline';
-  const bgClasses = twMerge(
-    BACKGROUND_VARIANTS[styleType][variantColor],
+  const sizeClasses = SIZE[size];
+  const baseClasses = 'rounded-[12px] font-semibold cursor-pointer';
+  const variantClasses =
+    fillType === 'solid'
+      ? `${COLOR_STYLES[themeColor][0]} text-white`
+      : `bg-white border ${COLOR_STYLES[themeColor][1]} ${COLOR_STYLES[themeColor][2]}`;
+
+  const buttonClassName = twMerge(
+    sizeClasses,
+    baseClasses,
+    variantClasses,
     disabled && 'cursor-not-allowed',
   );
-
-  const buttonClassName = twMerge(sizeClasses, baseClasses, bgClasses);
 
   return (
     <button {...buttonProps} className={buttonClassName}>
