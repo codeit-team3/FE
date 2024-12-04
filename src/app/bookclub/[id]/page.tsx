5a46b9da82e71292b2c16a76b1f7cf43915f7d53
cloud0406 <cloud0406@naver.com>
2024-12-04 09:18:36 +0900

    💄[Design] 카드 상단 영역

--- src/app/bookclub/[id]/page.tsx
+++ src/app/bookclub/[id]/page.tsx
@@ -1,4 +1,5 @@
 import BorderedBox from '@/components/bordered-box/BorderedBox';
+import { TextChip } from '@/components/text-chip/TextChip';
 import Image from 'next/image';
 
 function BookClubDetailPage({}) {
@@ -17,9 +18,46 @@ function BookClubDetailPage({}) {
       </BorderedBox>
 
       {/* 모임 정보 */}
-      <div className="flex flex-col bg-white p-4 shadow-md">
-        <div>모임 정보 영역</div>
-      </div>
+      <BorderedBox>
+        <div className="flex w-full flex-col">
+          {/* 상단 정보 영역 */}
+          <div className="flex justify-between px-6 pb-6">
+            {/* 왼쪽: 모임 정보 */}
+            <div className="flex flex-col gap-3">
+              <div className="flex flex-col gap-0.5">
+                <h1 className="text-lg font-semibold text-gray-900">
+                  달램핏 오피스 스트레칭
+                </h1>
+                <p className="text-sm font-medium text-gray-700">
+                  을지로 3가 서울시 중구 청계천로 100
+                </p>
+              </div>
+              <div className="flex gap-2">
+                <TextChip text="1월 7일" />
+                <TextChip text="17:30" isDueSoon />
+              </div>
+            </div>
+
+            {/* 오른쪽: 하트 아이콘 */}
+            <div className="text-gray-300">♡</div>
+          </div>
+
+          {/* 구분선 */}
+          <hr className="mx-1 border-t-2 border-dashed border-gray-200" />
+
+          {/* 하단 정보 영역 */}
+          <div className="flex w-full flex-col gap-2 px-6 pt-3">
+            <div className="flex flex-col gap-3">
+              <div>정원 + 아바타</div>
+              <div>프로그레스바</div>
+            </div>
+            <div className="flex w-full items-center justify-between">
+              <span>최소 인원</span>
+              <span>최대 인원</span>
+            </div>
+          </div>
+        </div>
+      </BorderedBox>
 
       {/* 모임 상세 설명 */}
       <div className="flex flex-col bg-white p-4 shadow-md">
