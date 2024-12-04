cc235d608825f1489b05030014db96641768e9a6
sunnwave <ssun20886@gmail.com>
2024-12-04 14:13:55 +0900

    ✅[Test] 임시 테스트코드 수정 #51

--- src/components/drop-down/DropDown.test.tsx
+++ src/components/drop-down/DropDown.test.tsx
@@ -3,8 +3,8 @@ import DropDown from './DropDown';
 import { render, screen } from '@testing-library/react';
 
 describe('DropDown', () => {
-  it('variant:navbar 일 때 렌더링 확인', () => {
-    render(<DropDown variant="navbar" items={['마이페이지, 로그아웃']} />);
+  it('variant:sorting 일 때 렌더링 확인', () => {
+    render(<DropDown variant="sorting" items={['최신순, 오래된순']} />);
     expect(screen.getByRole('button'));
   });
 });
