25393057204cfd85996ac3167b7f80268046e355
cloud0406 <cloud0406@naver.com>
2024-12-04 11:35:27 +0900

    ♻️[Refactor] avatar 컴포넌트 코드 수정

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -13,18 +13,15 @@ const SIZE = {
   lg: 'h-[56px] w-[56px]',
 } as const;
 
-export default function Avatar({
-  src,
-  alt,
-  size = 'sm',
-  ...props
-}: AvatarProps) {
+function Avatar({ src, alt, size = 'sm', ...props }: AvatarProps) {
   return (
     <div
-      className={`relative overflow-hidden rounded-full ${SIZE[size]} ${props.className || 'https://picsum.photos/200/300?random=1'}`}
+      className={`relative overflow-hidden rounded-full ${SIZE[size]} ${props.className || ''}`}
       {...props}
     >
       <Image src={src} alt={alt} fill className="object-cover" />
     </div>
   );
 }
+
+export default Avatar;
