29dc80ac69101c0f0bb9a1e221b00b01292f2b45
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-12-07 19:46:25 +0900

    💄[Design] 네비게이션 버튼 클릭시 색상 변경 #9

--- src/components/header/HeaderBar.tsx
+++ src/components/header/HeaderBar.tsx
@@ -2,21 +2,22 @@
 
 import React from 'react';
 import NavButton from './NavButton';
+import { NAV_ITEMS } from '@/constants/navigation';
+import { usePathname } from 'next/navigation';
 
 function HeaderBar() {
-  const navItems = [
-    { href: '/exchange', label: 'bookco' },
-    { href: '/bookclub', label: '책 모임' },
-    { href: '/exchange', label: '책 교환' },
-    { href: '/wish', label: '찜 목록' },
-  ];
+  const pathname = usePathname();
 
   return (
     <nav className="fixed top-0 w-full bg-green-normal px-4 text-white md:px-7">
       <div className="mx-auto flex h-14 max-w-[1200px] items-center justify-between">
         <div className="flex items-center gap-4 md:gap-6">
-          {navItems.map((item) => (
-            <NavButton key={item.href} href={item.href}>
+          {NAV_ITEMS.map((item) => (
+            <NavButton
+              key={item.id}
+              href={item.href}
+              isActive={item.id === 'bookco' || pathname === item.href}
+            >
               {item.label}
             </NavButton>
           ))}
