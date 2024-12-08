06e46d89e5ad18d08fedb8845bf5fecab38c85ce
wynter24 <m.wynter24.k@gmail.com>
2024-12-08 18:19:11 +0900

    ✨[Feat] FilterCheckbox 컴포넌트 생성 #73

--- src/components/filter-checkbox/FilterCheckbox.tsx
+++ src/components/filter-checkbox/FilterCheckbox.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
+import { IcCheck } from '../../../public/icons';
 
-interface filterCheckboxProps extends React.ComponentPropsWithoutRef<'input'> {
+interface FilterCheckboxProps extends React.ComponentPropsWithoutRef<'input'> {
   label: string;
 }
 
-function FilterCheckbox({ label, ...props }: filterCheckboxProps) {
+function FilterCheckbox({ label, ...props }: FilterCheckboxProps) {
+  const checked = props.checked;
   return (
-    <label className="flex cursor-pointer items-center gap-x-2 rounded-xl border border-gray-normal-hover px-2 py-1">
-      <span className="font-gray-dark-hover text-sm font-medium">{label}</span>
-      <input type="checkbox" id="checkbox" {...props} className="hidden" />
+    <label
+      className={`inline-flex h-10 w-fit min-w-24 cursor-pointer items-center gap-x-1 rounded-xl border bg-white px-[14px] py-2 transition duration-200 ${
+        checked ? 'border-green-normal' : 'border-gray-normal-hover'
+      }`}
+    >
+      <span
+        className={`text-sm font-medium ${
+          checked ? 'text-green-normal' : 'text-gray-dark-hover'
+        }`}
+      >
+        {label}
+      </span>
+      <div className="relative h-5 w-5">
+        <input
+          type="checkbox"
+          id="checkbox"
+          {...props}
+          className="peer absolute h-5 w-5 appearance-none rounded-[5px] border-2 border-gray-normal-hover bg-transparent transition duration-200 checked:border-green-normal checked:bg-green-normal"
+        />
+        <IcCheck
+          className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform transition duration-200 ${
+            checked ? 'opacity-100' : 'opacity-0'
+          }`}
+          width={28}
+          height={28}
+          circleColor="none"
+          strokeColor="#e6f6f4"
+        />
+      </div>
     </label>
   );
 }
+
 export default FilterCheckbox;
