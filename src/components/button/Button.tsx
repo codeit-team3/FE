514baf97085e1305e4280ba1335ab15e72f0b13d
wynter24 <m.wynter24.k@gmail.com>
2024-12-10 01:49:03 +0900

    💄[Design] isSubmitting props 추가 및 관련 스타일 추가 #99

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -5,18 +5,28 @@ interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
   text: string;
   size: 'large' | 'small' | 'modal';
   fillType: 'solid' | 'bordered';
-  themeColor: 'orange-600' | 'gray-400';
+  themeColor: 'green-normal-01' | 'gray-normal-03';
+  isSubmitting?: boolean;
 }
 
 const SIZE = {
   modal: 'min-w-[120px] h-[44px] px-4 text-base',
-  large: 'min-w-[332px] h-[44px] px-4 text-base',
+  large: 'min-w-[402px] h-[44px] px-4 text-base',
+  medium: 'min-w-[332px] h-[44px] px-4 text-base',
   small: 'min-w-[120px] h-[40px] px-3 text-sm',
 } as const;
 
 const COLOR_STYLES = {
-  'orange-600': ['bg-orange-600', 'text-orange-600', 'border-orange-600'],
-  'gray-400': ['bg-gray-400', 'text-gray-400', 'border-gray-400'],
+  'green-normal-01': [
+    'bg-green-normal-01',
+    'text-green-normal-01',
+    'border-green-normal-01',
+  ],
+  'gray-normal-03': [
+    'bg-gray-normal-03',
+    'text-gray-normal-03',
+    'border-gray-normal-03',
+  ],
 } as const;
 
 export default function Button({
@@ -24,26 +34,41 @@ export default function Button({
   size,
   fillType,
   themeColor,
+  isSubmitting,
   ...buttonProps
 }: ButtonProps) {
   const { disabled } = buttonProps;
 
   const sizeClasses = SIZE[size];
   const baseClasses = 'rounded-[12px] font-semibold cursor-pointer';
+
+  const resolvedColor =
+    isSubmitting !== undefined
+      ? isSubmitting
+        ? 'gray-normal-03'
+        : 'green-normal-01'
+      : themeColor;
+
   const variantClasses =
     fillType === 'solid'
-      ? `${COLOR_STYLES[themeColor][0]} text-white`
-      : `bg-white border ${COLOR_STYLES[themeColor][1]} ${COLOR_STYLES[themeColor][2]}`;
+      ? `${COLOR_STYLES[resolvedColor][0]} text-white`
+      : `bg-white border ${COLOR_STYLES[resolvedColor][1]} ${COLOR_STYLES[resolvedColor][2]}`;
+
+  const isButtonDisabled = isSubmitting || disabled;
 
   const buttonClassName = twMerge(
     sizeClasses,
     baseClasses,
     variantClasses,
-    disabled && 'cursor-not-allowed',
+    isButtonDisabled && 'cursor-not-allowed',
   );
 
   return (
-    <button {...buttonProps} className={buttonClassName}>
+    <button
+      {...buttonProps}
+      className={buttonClassName}
+      disabled={isButtonDisabled} // 제출 중이거나 외부에서 disabled가 true일 경우 비활성화
+    >
       {text}
     </button>
   );
