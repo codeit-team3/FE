96d6a1b0f2fa89b8b95f997ffce2747b4157f316
sun <104830526+sunnwave@users.noreply.github.com>
2024-12-10 09:43:14 +0900

    ✨[Feat] Dropdown 컴포넌트 onclick 이벤트 구현 (#108)
    * ✅[Test] 드롭다운 스토리북 테스트코드 구현 #48
    
    * 💄[Design] DropDown UI 구현#51
    
    * ♻️[Refactor] variant에 filtering 추가. 드롭다운 아이템 리스트 props로 입력받도록 변경. variant에 따른 UI 변경 코드 리팩토링 #51
    
    * 💄[Design] navbar 프로필 이미지 추가 #51
    
    * ♻️[Refactor] IcSorting color props로 입력받을 수 있도록 수정 #51
    
    * 💄[Design] small 사이즈 규격 변경, IcSorting svg path 변경 #51
    
    * 💬[Comment] 주석 정리 #51
    
    * ✅[Test] PR 등록을 위한 임시 테스트 코드 작성 #51
    
    * ✅[Test] 임시 테스트코드 수정 #51
    
    * 🔥[Remove] sorting-button 컴포넌트 폴더 삭제 #51
    
    * ✨[Feat] 드롭다운 아이템 이외의 요소 클릭 시 드롭다운 접기 기능 구현
    
    * ✨[Feat] 드롭다운 아이템 클릭 시 버튼 라벨을 해당 아이템 라벨로 변경되도록 구현 #68
    
    * ♻️[Refactor] variant 별 dropdown 아이템 리스트를 props로 입력받지 않고  constants로 분리 #68
    
    * 🎨[Style] 변수명 오타 수정 #68
    
    * ✅[Test] DropDown 클릭이벤트 테스트코드 작성 #68
    
    * ♻️[Refactor] useDropDownClose와 onClickDropDownItem에서 setIsOpen(false)로 드롭다운 상태 명시적으로 변경 #68
    
    * 💬[Comment] 테스트코드 주석 추가 #68
    
    * ✅[Test] 테스트코드 수정 #68
    
    * 💬[Comment] 주석 정리 #68

--- src/components/drop-down/DropDown.test.tsx
+++ src/components/drop-down/DropDown.test.tsx
@@ -1,18 +1,76 @@
 import '@testing-library/jest-dom';
 import DropDown from './DropDown';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MENU_ITEMS } from '@/constants';
 
-describe('DropDown', () => {
-  it('variant:filtering 일 때 렌더링 확인', () => {
+describe('DropDown variant:navbar 일 때', () => {
+  const mockHandleSelectionChange = jest.fn();
+
+  it('드롭다운 버튼 클릭 이벤트로 드롭다운 메뉴 아이템 렌더링 확인', () => {
     render(
       <DropDown
-        variant="filtering"
-        items={[
-          { value: 1, label: '온라인' },
-          { value: 2, label: '오프라인' },
-        ]}
+        variant="navbar"
+        onChangeSelection={mockHandleSelectionChange}
       />,
     );
-    expect(screen.getByRole('button'));
+    //드롭다운 버튼 렌더링 확인
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+
+    //유저가 드롭다운 클릭 전엔 메뉴 아이템들이 나타나지 않음
+    const menuItems = screen.queryByRole('listbox');
+    expect(menuItems).not.toBeInTheDocument();
+
+    //유저가 드롭다운 클릭 후엔 메뉴 아이템들이 나타남
+    userEvent.click(button);
+    expect(menuItems).toBeInTheDocument;
+  });
+
+  it('드롭다운 메뉴 아이템 클릭 시 해당 아이템 value 값 반환', async () => {
+    const user = userEvent.setup();
+    render(
+      <DropDown
+        variant="navbar"
+        onChangeSelection={mockHandleSelectionChange}
+      />,
+    );
+
+    const button = screen.getByRole('button');
+    //유저가 드롭다운 클릭 전엔 메뉴 아이템들이 나타나지 않음
+    const menuItems = screen.queryByRole('listbox');
+    expect(menuItems).not.toBeInTheDocument();
+
+    //유저가 드롭다운 클릭 후엔 메뉴 아이템들이 나타남
+    userEvent.click(button);
+    expect(menuItems).toBeInTheDocument;
+
+    //유저가 메뉴 아이템의 label을 보고 클릭하면 메뉴 아이템의 value값과 handle 함수를 호출
+    const menuItem = screen.getByText(MENU_ITEMS['navbar'][0].label);
+    await user.click(menuItem);
+
+    expect(mockHandleSelectionChange).toHaveBeenCalledWith(
+      MENU_ITEMS['navbar'][0].value,
+    );
+  });
+
+  it('드롭다운 외부 영역을 클릭했을 때 드롭다운 메뉴가 닫히는지 확인', async () => {
+    const user = userEvent.setup();
+    render(
+      <>
+        <DropDown
+          variant="navbar"
+          onChangeSelection={mockHandleSelectionChange}
+        />
+        <div>드롭다운 외부 영역</div>
+      </>,
+    );
+
+    const button = screen.getByRole('button');
+    user.click(button);
+    expect(button).toBeInTheDocument();
+
+    user.click(screen.getByText('드롭다운 외부 영역'));
+    expect(screen.queryByRole('listbox')).not.toBeInTheDocument();
   });
 });
