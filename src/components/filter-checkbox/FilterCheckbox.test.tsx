d00d5555c73a756f80186fffd28e60299197168a
wynter24 <m.wynter24.k@gmail.com>
2024-12-10 12:57:20 +0900

    ✅[Test] 불필요한 스타일 관련 테스트 제거 #73

--- src/components/filter-checkbox/FilterCheckbox.test.tsx
+++ src/components/filter-checkbox/FilterCheckbox.test.tsx
@@ -1,34 +1,11 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import FilterCheckbox from './FilterCheckbox';
 
 describe('FilterCheckbox 컴포넌트', () => {
-  it('체크 상태에 따라 올바른 스타일이 적용된다', () => {
-    const { rerender } = render(
-      <FilterCheckbox label="신청가능" checked={false} onChange={() => {}} />,
-    );
-
-    const checkbox = screen.getByRole('checkbox');
-    const label = screen.getByText('신청가능');
-
-    expect(checkbox).toHaveClass('border-gray-dark-02');
-    expect(label).toHaveClass('text-gray-dark-02');
-    expect(checkbox).not.toBeChecked();
-
-    rerender(
-      <FilterCheckbox label="신청가능" checked={true} onChange={() => {}} />,
-    );
-
-    expect(checkbox).toBeChecked();
-
-    checkbox.click();
-    expect(checkbox).toHaveClass('checked:border-green-normal-01');
-    expect(label).toHaveClass('text-green-normal-01');
-  });
-
-  it('체크박스를 클릭하면 onChange 핸들러가 호출된다', () => {
+  it('체크박스를 클릭하면 onChange 핸들러가 호출된다', async () => {
     const mockOnChange = jest.fn();
-    render(
+    const { debug } = render(
       <FilterCheckbox
         label="신청가능"
         checked={false}
@@ -39,6 +16,10 @@ describe('FilterCheckbox 컴포넌트', () => {
     const checkbox = screen.getByRole('checkbox');
 
     fireEvent.click(checkbox);
-    expect(mockOnChange).toHaveBeenCalledTimes(1);
+    await waitFor(() => {
+      expect(mockOnChange).toHaveBeenCalledTimes(1);
+      expect(checkbox).toBeChecked();
+      debug();
+    });
   });
 });
