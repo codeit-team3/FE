2116b8a9f128ad3d3f6a984ae2e16ec9c82326a6
cloud0406 <cloud0406@naver.com>
2024-12-10 18:28:30 +0900

    ✨[Feat] 라디오 버튼 영역 컴포넌트화

--- src/app/bookclub/create/page.tsx
+++ src/app/bookclub/create/page.tsx
@@ -4,7 +4,8 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { useState } from 'react';
-import { CreateClubFormField } from '@/features/club-create/components';
+import CreateClubFormField from '@/features/club-create/components/CreateClubFormField';
+import RadioButtonGroup from '@/features/club-create/components/RadioButtonGroup';
 import Button from '@/components/button/Button';
 
 const bookClubSchema = z.object({
@@ -68,6 +69,7 @@ export default function CreateBookClub() {
             placeholder="지정책인 경우, 책 이름을 넣어주세요"
           />
         </CreateClubFormField>
+
         <CreateClubFormField
           label="모임 상세 설명"
           error={errors.description?.message}
@@ -80,6 +82,7 @@ export default function CreateBookClub() {
             placeholder="상세 설명을 입력해주세요"
           />
         </CreateClubFormField>
+
         <CreateClubFormField label="이미지" error={errors.image?.message}>
           <div className="flex w-full items-center gap-2">
             <input
@@ -116,190 +119,34 @@ export default function CreateBookClub() {
         </CreateClubFormField>
 
         <CreateClubFormField label="자유책 / 지정책">
-          <div className="flex gap-4 md:gap-6">
-            <label
-              className={`flex w-full cursor-pointer items-start gap-2 rounded-xl p-4 ${
-                watch('bookType') === '자유책'
-                  ? 'border-2 border-green-normal-01'
-                  : ''
-              } bg-gray-light-02`}
-            >
-              <input
-                type="radio"
-                id="freeBook"
-                value="자유책"
-                {...register('bookType')}
-                className="hidden"
-              />
-              <div
-                className={`flex h-6 w-6 items-center justify-center rounded border bg-white ${
-                  watch('bookType') === '자유책'
-                    ? 'border-green-normal-01'
-                    : 'border-gray-normal-02'
-                }`}
-              >
-                {watch('bookType') === '자유책' && (
-                  <svg
-                    className="h-4 w-4 text-green-normal-01"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={4} // 체크 두께 설정
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M5 13l4 4L19 7"
-                    />
-                  </svg>
-                )}
-              </div>
-              <div className="flex flex-col gap-0.5">
-                <p className="font-semibold">자유책</p>
-                <p className="text-xs font-medium text-gray-dark-02">
-                  읽고 싶은 책을 자유롭게 선택하고 각자의 생각을 나눠요.
-                </p>
-              </div>
-            </label>
-
-            <label
-              className={`flex w-full cursor-pointer items-start gap-2 rounded-xl p-4 ${
-                watch('bookType') === '지정책'
-                  ? 'border-2 border-green-normal-01'
-                  : ''
-              } bg-gray-light-02`}
-            >
-              <input
-                type="radio"
-                id="designatedBook"
-                value="지정책"
-                {...register('bookType')}
-                className="hidden"
-              />
-              <div
-                className={`flex h-6 w-6 items-center justify-center rounded border bg-white ${
-                  watch('bookType') === '지정책'
-                    ? 'border-green-normal-01'
-                    : 'border-gray-normal-02'
-                }`}
-              >
-                {watch('bookType') === '지정책' && (
-                  <svg
-                    className="h-4 w-4 text-green-normal-01"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={4} // 체크 두께 설정
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M5 13l4 4L19 7"
-                    />
-                  </svg>
-                )}
-              </div>
-
-              <div className="flex flex-col gap-0.5">
-                <p className="font-semibold">지정책</p>
-                <p className="text-xs font-medium text-gray-dark-02">
-                  한 권의 책을 선정해 깊이 있는 토론을 진행해요.
-                </p>
-              </div>
-            </label>
-          </div>
+          <RadioButtonGroup
+            options={[
+              {
+                label: '자유책',
+                value: '자유책',
+                description:
+                  '읽고 싶은 책을 자유롭게 선택하고 각자의 생각을 나눠요.',
+              },
+              {
+                label: '지정책',
+                value: '지정책',
+                description: '한 권의 책을 선정해 깊이 있는 토론을 진행해요.',
+              },
+            ]}
+            selectedValue={watch('bookType')}
+            register={register('bookType')}
+          />
         </CreateClubFormField>
 
         <CreateClubFormField label="온라인 / 오프라인">
-          <div className="flex gap-4 md:gap-6">
-            <label
-              className={`flex w-full cursor-pointer items-start gap-2 rounded-xl p-4 ${
-                watch('location') === '온라인'
-                  ? 'border-2 border-green-normal-01'
-                  : ''
-              } bg-gray-light-02`}
-            >
-              <input
-                type="radio"
-                id="online"
-                value="온라인"
-                {...register('location')}
-                className="hidden"
-              />
-              <div
-                className={`flex h-6 w-6 items-center justify-center rounded border bg-white ${
-                  watch('location') === '온라인'
-                    ? 'border-green-normal-01'
-                    : 'border-gray-normal-02'
-                }`}
-              >
-                {watch('location') === '온라인' && (
-                  <svg
-                    className="h-4 w-4 text-green-normal-01"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={4} // 체크 두께 설정
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M5 13l4 4L19 7"
-                    />
-                  </svg>
-                )}
-              </div>
-              <div className="flex flex-col gap-0.5">
-                <p className="font-semibold">온라인</p>
-              </div>
-            </label>
-
-            <label
-              className={`flex w-full cursor-pointer items-start gap-2 rounded-xl p-4 ${
-                watch('location') === '오프라인'
-                  ? 'border-2 border-green-normal-01'
-                  : ''
-              } bg-gray-light-02`}
-            >
-              <input
-                type="radio"
-                id="offline"
-                value="오프라인"
-                {...register('location')}
-                className="hidden"
-              />
-              <div
-                className={`flex h-6 w-6 items-center justify-center rounded border bg-white ${
-                  watch('location') === '오프라인'
-                    ? 'border-green-normal-01'
-                    : 'border-gray-normal-02'
-                }`}
-              >
-                {watch('location') === '오프라인' && (
-                  <svg
-                    className="h-4 w-4 text-green-normal-01"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={4} // 체크 두께 설정
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M5 13l4 4L19 7"
-                    />
-                  </svg>
-                )}
-              </div>
-              <div className="flex flex-col gap-0.5">
-                <p className="font-semibold">오프라인</p>
-              </div>
-            </label>
-          </div>
+          <RadioButtonGroup
+            options={[
+              { label: '온라인', value: '온라인' },
+              { label: '오프라인', value: '오프라인' },
+            ]}
+            selectedValue={watch('location')}
+            register={register('location')}
+          />
         </CreateClubFormField>
 
         <CreateClubFormField
@@ -312,6 +159,7 @@ export default function CreateBookClub() {
             className="w-full rounded-xl border bg-gray-light-02 px-4 py-[10px] font-medium placeholder-gray-dark-02"
           />
         </CreateClubFormField>
+
         <CreateClubFormField
           label="언제 모임을 마감할까요?"
           error={errors.endDate?.message}
@@ -322,6 +170,7 @@ export default function CreateBookClub() {
             className="w-full rounded-xl border bg-gray-light-02 px-4 py-[10px] font-medium placeholder-gray-dark-02"
           />
         </CreateClubFormField>
+
         <CreateClubFormField
           label="모임 정원"
           error={errors.maxParticipants?.message}
@@ -335,6 +184,7 @@ export default function CreateBookClub() {
             className="w-full rounded-xl border bg-gray-light-02 px-4 py-[10px] font-medium placeholder-gray-dark-02"
           />
         </CreateClubFormField>
+
         <Button
           type="submit"
           text="확인"
---
+++ src/features/club-create/components/RadioButtonGroup.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+interface RadioButtonGroupProps {
+  options: { label: string; value: string; description?: string }[];
+  selectedValue: string;
+  register: any;
+}
+
+const RadioButtonGroup: React.FC<RadioButtonGroupProps> = ({
+  options,
+  selectedValue,
+  register,
+}) => (
+  <div className="flex gap-4 md:gap-6">
+    {options.map((option) => (
+      <label
+        key={option.value}
+        className={`flex w-full cursor-pointer items-start gap-2 rounded-xl p-4 ${
+          selectedValue === option.value
+            ? 'border-2 border-green-normal-01'
+            : ''
+        } bg-gray-light-02`}
+      >
+        <input
+          type="radio"
+          value={option.value}
+          {...register}
+          className="hidden"
+        />
+        <div
+          className={`flex h-6 w-6 items-center justify-center rounded border bg-white ${
+            selectedValue === option.value
+              ? 'border-green-normal-01'
+              : 'border-gray-normal-02'
+          }`}
+        >
+          {selectedValue === option.value && (
+            <svg
+              className="h-4 w-4 text-green-normal-01"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={4}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5 13l4 4L19 7"
+              />
+            </svg>
+          )}
+        </div>
+        <div className="flex flex-col gap-0.5">
+          <p className="font-semibold">{option.label}</p>
+          {option.description && (
+            <p className="text-xs font-medium text-gray-dark-02">
+              {option.description}
+            </p>
+          )}
+        </div>
+      </label>
+    ))}
+  </div>
+);
+
+export default RadioButtonGroup;
