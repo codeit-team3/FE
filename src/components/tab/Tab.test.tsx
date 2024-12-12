0076ac29e128b32b4a7a23c40416d9dcce3c43bc
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-12-12 09:16:11 +0900

    💄[Design] Sub_Tab 디자인 변경 (#136)
    * 💄[Design] 버튼 색상 추가 #134
    
    * ♻️[Refactor] SubTab 디자인 변경 #134

--- src/components/tab/Tab.test.tsx
+++ src/components/tab/Tab.test.tsx
@@ -25,20 +25,6 @@ describe('Tab 컴포넌트', () => {
     });
   });
 
-  it('활성화된 탭은 올바른 스타일이 적용되어야 한다', () => {
-    render(
-      <Tab
-        items={items}
-        activeTab="탭1"
-        onTabChange={mockOnTabChange}
-        tabType="MAIN_TAB"
-      />,
-    );
-
-    const activeTab = screen.getByText('탭1');
-    expect(activeTab).toHaveClass('border-green-dark-01', 'text-green-dark-01');
-  });
-
   it('탭 클릭시 onTabChange가 호출되어야 한다', () => {
     render(
       <Tab
@@ -53,29 +39,5 @@ describe('Tab 컴포넌트', () => {
     fireEvent.click(secondTab);
 
     expect(mockOnTabChange).toHaveBeenCalledWith('탭2');
-  });
-
-  it('tabType에 따라 올바른 텍스트 크기가 적용되어야 한다', () => {
-    const { rerender } = render(
-      <Tab
-        items={items}
-        activeTab="탭1"
-        onTabChange={mockOnTabChange}
-        tabType="MAIN_TAB"
-      />,
-    );
-
-    expect(screen.getByText('탭1')).toHaveClass('text-xl');
-
-    rerender(
-      <Tab
-        items={items}
-        activeTab="탭1"
-        onTabChange={mockOnTabChange}
-        tabType="SUB_TAB"
-      />,
-    );
-
-    expect(screen.getByText('탭1')).toHaveClass('text-lg');
   });
 });
