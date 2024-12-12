10eb4ae49da9c278ec38680c04f23d41658ad2a3
wynter24 <m.wynter24.k@gmail.com>
2024-12-12 22:49:52 +0900

    ✨[Feat] 책모임 FilterBaR 컴포넌트 생성 #132

--- src/features/bookclub/components/index.ts
+++ src/features/bookclub/components/index.ts
@@ -1,3 +1,4 @@
 export { default as BookClubMainPage } from './BookClubMainPage';
 export { default as Header } from './Header';
 export { default as Main } from './Main';
+export { default as FilterBar } from './FilterBar';
