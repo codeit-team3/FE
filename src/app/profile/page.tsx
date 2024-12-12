6d35f91b5f530aae3655f7bce5b0604110256f46
sunnwave <ssun20886@gmail.com>
2024-12-12 16:27:20 +0900

    💄[Design] 프로필페페이지 레이아웃 설정 #119

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="mx-[86px] mt-[50px] flex-col">
+      <div className="" role="upper">
+        <div className="border text-xl font-bold text-gray-black">
+          마이 페이지
+        </div>
+        <div className="mx-[5px] my-[20px] h-[200px] border">프로필</div>
+      </div>
+      <div className="border" role="container">
+        <div className="border">
+          메인탭
+          <div className="border">버튼 모음</div>
+          <div className="border">카드 컴포넌트 리스트</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
