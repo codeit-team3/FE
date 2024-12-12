8310dc76ebea3a0574f512b1aa41293df86cd53c
wynter24 <m.wynter24.k@gmail.com>
2024-12-12 12:10:17 +0900

    🐛[Fix] normalize line endings #72

--- src/app/bookclub/page.tsx
+++ src/app/bookclub/page.tsx
@@ -1,3 +1,5 @@
+// import BookClubMainPage from '@/features/bookclub/components/BookClubMainPage';
+
 export default function Home() {
-  return <div></div>;
+  return <div className="-mx-4 md:-mx-6 lg:-mx-10"></div>;
 }
