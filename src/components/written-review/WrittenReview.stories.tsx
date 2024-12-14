fc6a6fafc6d289f807fa65759fc20f17882e3833
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-12-14 17:36:34 +0900

    ✨[Feat] WrittenReview를 활용한 ClubReview 컴포넌트 생성 #154 (#156)
    * 💄[Design] nav, 공통 레이아웃 수정 #154
    
    * ✨[Feat] ClubReview 컴포넌트 생성 #154
    
    * ✅[Test] WrittenReview 스토리북 수정 #154

--- src/components/written-review/WrittenReview.stories.tsx
+++ src/components/written-review/WrittenReview.stories.tsx
@@ -15,38 +15,61 @@ interface StoryProps {
 }
 
 const meta = {
-  title: 'Components/WrittenReview',
+  title: 'Components/WrittenReview/Base',
   component: WrittenReview,
   parameters: {
     layout: 'centered',
+    docs: {
+      description: {
+        component:
+          'WrittenReview 컴포넌트는 컴파운드 패턴으로 구성된 리뷰를 렌더링하는 데 사용됩니다. 하위 컴포넌트는 독립적으로도 활용할 수 있습니다.',
+      },
+    },
   },
 } satisfies Meta<typeof WrittenReview>;
 
 export default meta;
 type Story = StoryObj<StoryProps>;
 
-export const FindClubReview: Story = {
-  render: (args) => (
-    <WrittenReview>
-      <div className="flex flex-col gap-y-2">
-        <WrittenReview.Rating ratingCount={args.ratingCount} />
-        <WrittenReview.Comment text={args.comment} />
-        <WrittenReview.UserProfile
-          profileImage={args.profileImage}
-          userName={args.userName}
-          createdAt={args.createdAt}
-        />
-      </div>
-    </WrittenReview>
-  ),
+export const Rating: Story = {
+  render: (args) => <WrittenReview.Rating ratingCount={args.ratingCount} />,
   args: {
     ratingCount: 4,
+  },
+};
+
+export const Comment: Story = {
+  render: (args) => <WrittenReview.Comment text={args.comment} />,
+  args: {
     comment:
       '아침부터 자기발전을 위한 시간을 가져서 좋았어요. 각자의 길 위에서 달려가는 생생한 순간을 공유해주셔서 감사합니다!',
+  },
+};
+
+export const UserProfile: Story = {
+  render: (args) => (
+    <WrittenReview.UserProfile
+      profileImage={args.profileImage}
+      userName={args.userName}
+      createdAt={args.createdAt}
+    />
+  ),
+  args: {
     profileImage:
       'https://cdn.pixabay.com/photo/2024/02/17/00/18/cat-8578562_1280.jpg',
     userName: '다람쥐',
     createdAt: '2024.01.25',
+  },
+};
+
+export const ClubImage: Story = {
+  render: (args) => (
+    <WrittenReview.ClubImage src={args.clubImage} alt={args.clubImageAlt} />
+  ),
+  args: {
+    clubImage:
+      'https://cdn.pixabay.com/photo/2024/02/17/00/18/cat-8578562_1280.jpg',
+    clubImageAlt: '모임 이미지',
   },
 };
 
