13ad3c766d5ed14c53818af7dacbb4f0d64c12fe
sunnwave <ssun20886@gmail.com>
2024-12-15 02:37:38 +0900

    ♻️[Refactor] ProfilePage 컴포넌트 프레그먼트 제거 #153

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,11 +2,7 @@ import ProfilePage from '@/features/profile/components/ProfilePage';
 import React from 'react';
 
 const Profile = () => {
-  return (
-    <>
-      <ProfilePage />
-    </>
-  );
+  return <ProfilePage />;
 };
 
 export default Profile;
