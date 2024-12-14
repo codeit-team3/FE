4b4d3c7994c7c7b13b4b092448c36f3de6c5f05b
wynter24 <m.wynter24.k@gmail.com>
2024-12-14 22:46:50 +0900

    ♻️[Refactor] ClubReview 컴포넌트 사용 #147

--- src/features/club-details/components/ReviewListSection.tsx
+++ src/features/club-details/components/ReviewListSection.tsx
@@ -35,9 +35,21 @@
 // ];
 'use client';
 
+import ClubReview from '@/components/written-review/club-review/ClubReview';
 // import DropDown from '@/components/drop-down/DropDown';
-import WrittenReview from '@/components/written-review/WrittenReview';
 // import { useState } from 'react';
+
+const exampleReview = {
+  ratingCount: 4,
+  comment:
+    '아침부터 자기발전을 위한 시간을 가져서 좋았어요. 각자의 길 위에서 달려가는 생생한 순간을 공유해주셔서 감사합니다!',
+  userProfile: {
+    profileImage:
+      'https://cdn.pixabay.com/photo/2024/02/17/00/18/cat-8578562_1280.jpg',
+    userName: '다람쥐',
+    createdAt: '2024.01.25',
+  },
+};
 
 function ReviewListSection() {
   // 상위로 올려함
@@ -57,17 +69,11 @@ function ReviewListSection() {
         /> */}
       </div>
       <div className="flex flex-col gap-4 rounded-xl border-2 border-gray-normal-01 p-6">
-        <WrittenReview>
-          <div className="flex flex-col gap-y-2">
-            <WrittenReview.Rating ratingCount={4} />
-            <WrittenReview.Comment text="아침부터 자기발전을 위한 시간을 가져서 좋았어요. 각자의 길 위에서 달려가는 생생한 순간을 공유해주셔서 감사합니다!" />
-            <WrittenReview.UserProfile
-              profileImage="/images/profile.png"
-              userName="다람쥐"
-              createdAt="2024.01.25"
-            />
-          </div>
-        </WrittenReview>
+        <ClubReview
+          ratingCount={exampleReview.ratingCount}
+          comment={exampleReview.comment}
+          userProfile={exampleReview.userProfile}
+        />
       </div>
     </section>
   );
