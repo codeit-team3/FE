2cb49f08a255c810da9fa4426a6da181751e445d
wynter24 <m.wynter24.k@gmail.com>
2024-12-15 11:44:30 +0900

    📝[Docs] mockReview 수정 #147

--- src/features/club-details/components/ReviewListSection.tsx
+++ src/features/club-details/components/ReviewListSection.tsx
@@ -1,54 +1,52 @@
-// const MOCK_REVIEWS = [
-//   {
-//     id: '1',
-//     ratingCount: 4,
-//     comment: '두번째 이용이에요! 만족합니다.',
-//     userName: '동글동글이',
-//     createdAt: '2024.01.25',
-//     profileImage: '/images/profile.png',
-//   },
-//   {
-//     id: '2',
-//     ratingCount: 4,
-//     comment: '두번째 이용이에요! 만족합니다.',
-//     userName: '동글동글이',
-//     createdAt: '2024.01.25',
-//     profileImage: '/images/profile.png',
-//   },
-//   {
-//     id: '3',
-//     ratingCount: 3,
-//     comment:
-//       '강사분도 하시고 ~ ^^ 너무 좋은 공간에서 긴장과 스트레스 모두 잘 풀고 가요 ~ ^^',
-//     userName: '모닝러너',
-//     createdAt: '2024.01.25',
-//     profileImage: '/images/profile.png',
-//   },
-//   {
-//     id: '4',
-//     ratingCount: 3,
-//     comment: '수업이 단조로워요.',
-//     userName: '해보자고',
-//     createdAt: '2024.01.25',
-//     profileImage: '/images/profile.png',
-//   },
-// ];
 'use client';
 
 import ClubReview from '@/components/written-review/club-review/ClubReview';
 // import DropDown from '@/components/drop-down/DropDown';
 // import { useState } from 'react';
 
-const exampleReview = {
-  ratingCount: 4,
-  comment:
-    '아침부터 자기발전을 위한 시간을 가져서 좋았어요. 각자의 길 위에서 달려가는 생생한 순간을 공유해주셔서 감사합니다!',
-  userProfile: {
-    profileImage: '/images/profile.png',
-    userName: '다람쥐',
-    createdAt: '2024.01.25',
+const MOCK_REVIEWS = [
+  {
+    id: '1',
+    ratingCount: 4,
+    comment: '너무 좋았어요! 다음에도 참여할래요~',
+    userProfile: {
+      profileImage: '/images/profile.png',
+      userName: '다람쥐쥐',
+      createdAt: '2024.01.25',
+    },
   },
-};
+  {
+    id: '2',
+    ratingCount: 4,
+    comment: '두번째 이용이에요! 만족합니다.',
+    userProfile: {
+      profileImage: '/images/profile.png',
+      userName: '동글동글이',
+      createdAt: '2024.01.25',
+    },
+  },
+  {
+    id: '3',
+    ratingCount: 3,
+    comment:
+      '강사분도 하시고 ~ ^^ 너무 좋은 공간에서 긴장과 스트레스 모두 잘 풀고 가요 ~ ^^',
+    userProfile: {
+      profileImage: '/images/profile.png',
+      userName: '모닝러너',
+      createdAt: '2024.01.25',
+    },
+  },
+  {
+    id: '4',
+    ratingCount: 3,
+    comment: '수업이 단조로워요.',
+    userProfile: {
+      profileImage: '/images/profile.png',
+      userName: '해보자고',
+      createdAt: '2024.01.25',
+    },
+  },
+];
 
 function ReviewListSection() {
   // 상위로 올려함
@@ -68,12 +66,12 @@ function ReviewListSection() {
         /> */}
       </div>
       <div className="flex flex-col gap-4 rounded-xl border-2 border-gray-normal-01 p-6">
-        {Array.from({ length: 10 }).map((_, index) => (
+        {MOCK_REVIEWS.map((review, index) => (
           <ClubReview
             key={`review-${index}`}
-            ratingCount={exampleReview.ratingCount}
-            comment={exampleReview.comment}
-            userProfile={exampleReview.userProfile}
+            ratingCount={review.ratingCount}
+            comment={review.comment}
+            userProfile={review.userProfile}
           />
         ))}
       </div>
