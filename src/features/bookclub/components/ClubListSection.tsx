e3747efb12c94a984e8b8edfd27a3fb383bdfac7
sunnwave <ssun20886@gmail.com>
2024-12-16 11:47:14 +0900

    클럽 목록 조회 props 수정

--- src/features/bookclub/components/ClubListSection.tsx
+++ src/features/bookclub/components/ClubListSection.tsx
@@ -1,87 +1,100 @@
 'use client';
 
 import Card from '@/components/card/Card';
-import { CardProps } from '@/components/card/types';
+import { useRouter } from 'next/navigation';
 
-// const mockImgSrc = '/images/profile.png';
+const cardData = [
+  {
+    clubId: 1,
+    imageUrl: '/images/profile.png',
+    imageAlt: '모임 이미지',
+    title: '독서 모임 1',
+    location: '서울 강남구',
+    datetime: '2024-01-20 14:00',
+    isLiked: false,
+    current: 3,
+    max: 8,
+    isPast: false,
+    isCanceled: false,
+    clubStatus: 'pending' as const,
+    meetingType: 'OFFLINE' as const,
+    bookClubType: 'FIXED' as const,
 
-// const mockMeeting: Meeting = {
-//   meetingInfo: {
-//     title: '을지로에서 만나는 독서 모임',
-//     category: '자유책',
-//     location: '을지로 3가',
-//     datetime: '12/14(토) 오전 10:00',
-//   },
-//   participationStatus: {
-//     currentParticipants: 17,
-//     maxParticipants: 20,
-//     isConfirmed: true,
-//     confirmedVariant: 'confirmed',
-//     participants: [
-//       {
-//         src: mockImgSrc,
-//         alt: '참가자1',
-//       },
-//       {
-//         src: mockImgSrc,
-//         alt: '참가자2',
-//       },
-//       {
-//         src: mockImgSrc,
-//         alt: '참가자3',
-//       },
-//     ],
-//   },
-//   imageInfo: {
-//     url: mockImgSrc,
-//     isLiked: true,
-//     onLikeClick: () => alert('좋아요를 눌렀습니다!'),
-//   },
-//   isPast: false,
-//   isCanceled: false,
-//   actions: {
-//     onClick: () => alert('카드를 클릭했습니다!'),
-//     onDelete: () => alert('모임을 삭제했습니다!'),
-//   },
-// };
+    // meetingType: 'FIXED', // 정확한 리터럴 값 설정
+    // status: 'pending',
+  },
+  {
+    clubId: 2,
+    imageUrl: '/images/profile.png',
+    imageAlt: '모임 이미지',
+    title: '독서 모임 1',
+    location: '서울 강남구',
+    datetime: '2024-01-20 14:00',
+    isLiked: false,
+    current: 3,
+    max: 8,
+    isPast: false,
+    isCanceled: false,
+    clubStatus: 'pending' as const,
+    meetingType: 'OFFLINE' as const,
+    bookClubType: 'FIXED' as const,
 
-const defaultCardProps: CardProps = {
-  variant: 'defaultClub',
-  isLiked: false,
-  isCanceled: false,
-  current: 3,
-  max: 8,
-  imageUrl: '/images/profile.png',
-  imageAlt: '모임 이미지',
-  clubId: 45,
-  title: '독서 모임',
-  location: '서울 강남구',
-  datetime: '2024-01-20 14:00',
-  meetingType: 'OFFLINE',
-  bookClubType: 'FIXED',
-  isPast: false,
-  clubStatus: 'pending',
-  onLikeClick: () => {
-    console.log('좋아요 클릭');
+    // meetingType: 'FIXED', // 정확한 리터럴 값 설정
+    // status: 'pending',
   },
-  onClick: () => {
-    console.log('카드 클릭');
+  {
+    clubId: 3,
+    imageUrl: '/images/profile.png',
+    imageAlt: '모임 이미지',
+    title: '독서 모임 1',
+    location: '서울 강남구',
+    datetime: '2024-01-20 14:00',
+    isLiked: false,
+    current: 3,
+    max: 8,
+    isPast: false,
+    isCanceled: false,
+    clubStatus: 'pending' as const,
+    meetingType: 'OFFLINE' as const,
+    bookClubType: 'FIXED' as const,
+
+    // meetingType: 'FIXED', // 정확한 리터럴 값 설정
+    // status: 'pending',
   },
-  onDelete: () => {
-    console.log('삭제 클릭');
+  {
+    clubId: 4,
+    imageUrl: '/images/profile.png',
+    imageAlt: '모임 이미지',
+    title: '독서 모임 1',
+    location: '서울 강남구',
+    datetime: '2024-01-20 14:00',
+    isLiked: false,
+    current: 3,
+    max: 8,
+    isPast: false,
+    isCanceled: false,
+    clubStatus: 'pending' as const,
+    meetingType: 'OFFLINE' as const,
+    bookClubType: 'FIXED' as const,
+
+    // meetingType: 'FIXED', // 정확한 리터럴 값 설정
+    // status: 'pending',
   },
-};
+];
 
 function ClubListSection() {
+  const router = useRouter();
   return (
     <main className="flex w-full min-w-[336px] flex-col items-center gap-y-[26px] bg-gray-light-01 px-[20px] pt-[18px] sm:justify-between md:px-[24px] lg:px-[102px]">
       {cardData.map((card, index) => (
         <div className="md:w-full" key={index}>
           <Card
-            meetingType={'FIXED' as 'FIXED'}
-            status="pending"
             {...card}
-            onClick={() => router.push(`/bookclub/${card.id}`)}
+            // // meetingType={'FIXED' as 'FIXED'}
+            // // status="pending"
+            // {...card}
+            variant="defaultClub"
+            onClick={() => router.push(`/bookclub/${card.clubId}`)}
             onLikeClick={() => console.log('좋아요 클릭')}
             onDelete={() => console.log('삭제 클릭')}
           />
