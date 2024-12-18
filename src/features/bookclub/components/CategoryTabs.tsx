2f64740b864dd69e1c6770748a616b8bd5263fdf
wynter24 <m.wynter24.k@gmail.com>
2024-12-18 13:56:32 +0900

    ♻️[Refactor] bookClubType을 api와 ui 타입으로 분리 및 전환 #175

--- src/features/bookclub/components/CategoryTabs.tsx
+++ src/features/bookclub/components/CategoryTabs.tsx
@@ -1,5 +1,6 @@
 import Tab from '@/components/tab/Tab';
 import { Filters } from '../types/bookclubs';
+import { mapToApiBookClubType, mapToUiBookClubType } from '../utils/filters';
 
 interface CategoryTabsProps {
   filters: Filters;
@@ -7,12 +8,18 @@ interface CategoryTabsProps {
 }
 
 function CategoryTabs({ filters, onFilterChange }: CategoryTabsProps) {
+  const uiBookClubType = filters?.bookClubType
+    ? mapToUiBookClubType(filters.bookClubType)
+    : '전체';
+
   return (
     <div>
       <Tab
-        items={['ALL', 'FREE', 'FIXED'] as const}
-        activeTab={filters.bookClubType || 'ALL'}
-        onTabChange={(item) => onFilterChange({ bookClubType: item })}
+        items={['전체', '자유책', '지정책'] as const}
+        activeTab={uiBookClubType}
+        onTabChange={(item) =>
+          onFilterChange({ bookClubType: mapToApiBookClubType(item) })
+        }
         tabType="MAIN_TAB"
       />
       <hr className="-mx-[20px] border-t border-gray-normal-01 md:-mx-[24px] lg:-mx-[102px]" />
