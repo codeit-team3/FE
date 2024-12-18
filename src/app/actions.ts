14cab01b010bb10ad6d7b231bb7f04b78175d725
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-12-18 15:38:56 +0900

    🐛[Fix] middleware 버그 수정

---
+++ src/app/actions.ts
@@ -0,0 +1,52 @@
+'use server';
+
+import { LoginFormData } from '@/features/auth/types/loginFormSchema';
+import { cookies } from 'next/headers';
+
+export async function loginServer(data: LoginFormData) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auths/signin`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      },
+    );
+    const json = await response.json();
+    const { accessToken, refreshToken } = json;
+
+    const cookieStore = await cookies();
+    cookieStore.set('auth_token', accessToken);
+    cookieStore.set('refresh_token', refreshToken);
+    return json;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function logoutServer(accessToken: string, refreshToken: string) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auths/signout`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ accessToken, refreshToken }),
+      },
+    );
+    const cookieStore = await cookies();
+    cookieStore.delete('auth_token');
+    cookieStore.delete('refresh_token');
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
--- src/features/auth/api/auth.ts
+++ src/features/auth/api/auth.ts
@@ -1,34 +1,47 @@
 import apiClient from '@/lib/utils/apiClient';
 import { LoginFormData } from '../types/loginFormSchema';
 import { useAuthStore } from '@/store/authStore';
-import {
-  setCookie,
-  deleteCookie,
-  getCookie,
-} from '@/features/auth/utils/cookies';
+import { loginServer, logoutServer } from '@/app/actions';
+
+import { getCookie } from '@/features/auth/utils/cookies';
 import { User } from '../types/user';
 import { SignUpFormData } from '../types/sign-up.schema';
 
 export const login = async (data: LoginFormData) => {
   try {
-    const response = await apiClient.post<{
-      accessToken: string;
-      refreshToken: string;
-    }>('/auths/signin', data);
-
-    const { accessToken, refreshToken } = response.data;
-
-    setCookie('auth_token', accessToken);
-    setCookie('refresh_token', refreshToken);
-
+    const response = await loginServer.bind(null, data)();
     const { setIsLoggedIn } = useAuthStore.getState();
     setIsLoggedIn(true);
 
     await getUserInfo();
 
-    return response.data;
+    return response;
   } catch (error) {
     console.error('로그인 에러:', error);
+    throw error;
+  }
+};
+
+export const logout = async () => {
+  try {
+    const accessToken = getCookie('auth_token');
+    const refreshToken = getCookie('refresh_token');
+
+    if (!accessToken || !refreshToken) {
+      const { setIsLoggedIn, setUser } = useAuthStore.getState();
+      setIsLoggedIn(false);
+      setUser(null);
+      return;
+    }
+
+    const response = await logoutServer.bind(null, accessToken, refreshToken)();
+
+    const { setIsLoggedIn, setUser } = useAuthStore.getState();
+    setIsLoggedIn(false);
+    setUser(null);
+    return response;
+  } catch (error) {
+    console.error('로그아웃 에러:', error);
     throw error;
   }
 };
@@ -41,28 +54,6 @@ export const getUserInfo = async () => {
     return response.data;
   } catch (error) {
     console.error('사용자 정보 조회 에러:', error);
-    throw error;
-  }
-};
-
-export const logout = async () => {
-  try {
-    const accessToken = getCookie('auth_token');
-    const refreshToken = getCookie('refresh_token');
-
-    await apiClient.post('/auths/signout', {
-      accessToken,
-      refreshToken,
-    });
-
-    const { setIsLoggedIn, setUser } = useAuthStore.getState();
-    setIsLoggedIn(false);
-    setUser(null);
-
-    deleteCookie('auth_token');
-    deleteCookie('refresh_token');
-  } catch (error) {
-    console.error('로그아웃 에러:', error);
     throw error;
   }
 };
