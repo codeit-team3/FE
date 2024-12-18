4fc1727689f38dabd1a1d73bf1735c317e3033d7
wynter24 <m.wynter24.k@gmail.com>
2024-12-18 13:58:16 +0900

    ♻️[Refactor] 모임 만들기 페이지 이동 경로 추가 #175

--- src/features/bookclub/components/HeaderSection.tsx
+++ src/features/bookclub/components/HeaderSection.tsx
@@ -1,6 +1,9 @@
 import Button from '@/components/button/Button';
+import { useRouter } from 'next/navigation';
 
 function HeaderSection() {
+  const router = useRouter();
+
   return (
     <header className="flex h-[120px] w-full min-w-[336px] items-end bg-green-light-01 px-[20px] py-[30px] sm:justify-between md:px-[24px] lg:px-[102px]">
       <h1
@@ -15,6 +18,7 @@ function HeaderSection() {
         size="small"
         fillType="solid"
         themeColor="green-normal-01"
+        onClick={() => router.push('/bookclub/create')}
       />
     </header>
   );
