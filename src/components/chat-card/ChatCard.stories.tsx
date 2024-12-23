f086bfe8544a423ae98cbada1dfd5a1e7ed09bd7
cloud0406 <cloud0406@naver.com>
2024-12-23 15:07:48 +0900

    📝[Docs] 기본 atoms 스토리북

--- src/components/chat-card/ChatCard.stories.tsx
+++ src/components/chat-card/ChatCard.stories.tsx
@@ -14,64 +14,39 @@ export default meta;
 type Story = StoryObj<typeof ChatCard>;
 
 export const Box: Story = {
-  args: {
-    children: '기본 박스',
-  },
   render: () => (
-    <div className="flex gap-4">
-      <ChatCard.Box>기본 박스</ChatCard.Box>
-      <ChatCard.Box isHost>호스트 박스</ChatCard.Box>
-    </div>
+    <ChatCard.Box isHost>
+      <p>ChatCard Box Content</p>
+    </ChatCard.Box>
   ),
 };
 
 export const Title: Story = {
-  args: {
-    children: '채팅방 제목',
-  },
   render: () => <ChatCard.Title>채팅방 제목</ChatCard.Title>,
 };
 
 export const Image: Story = {
-  args: {},
   render: () => (
-    <div className="flex gap-4">
-      <ChatCard.Image url="https://picsum.photos/200" alt="채팅방 이미지" />
-      <ChatCard.Image
-        url="https://picsum.photos/200"
-        alt="호스트 채팅방 이미지"
-        isHost
-      />
-    </div>
+    <ChatCard.Image
+      url="https://picsum.photos/200"
+      isHost={true}
+      alt="채팅방 이미지"
+    />
   ),
 };
 
 export const Location: Story = {
-  args: {
-    children: '서울 강남구',
-  },
-  render: () => <ChatCard.Location>서울 강남구</ChatCard.Location>,
+  render: () => <ChatCard.Location>서울특별시 강남구</ChatCard.Location>,
 };
 
 export const DateTime: Story = {
-  args: {
-    children: '2024.03.15 토요일 15:00',
-  },
-  render: () => <ChatCard.DateTime>2024.03.15 토요일 15:00</ChatCard.DateTime>,
+  render: () => <ChatCard.DateTime>2024.03.15 (금) 19:00</ChatCard.DateTime>,
 };
 
 export const LastMessage: Story = {
-  args: {
-    children: '안녕하세요! 반갑습니다.',
-  },
-  render: () => (
-    <ChatCard.LastMessage>안녕하세요! 반갑습니다.</ChatCard.LastMessage>
-  ),
+  render: () => <ChatCard.LastMessage>반갑습니다~!</ChatCard.LastMessage>,
 };
 
 export const LastMessageTime: Story = {
-  args: {
-    children: '1시간 전',
-  },
-  render: () => <ChatCard.LastMessageTime>1시간 전</ChatCard.LastMessageTime>,
+  render: () => <ChatCard.LastMessageTime>10:29</ChatCard.LastMessageTime>,
 };
