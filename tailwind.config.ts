6844955a24d9c0eee7c7c5b1392def2fc073749d
cloud0406 <cloud0406@naver.com>
2024-12-30 16:50:10 +0900

    ✨[Feat] 호버 커스텀 유틸리티 클래스 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
@@ -52,7 +53,15 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        '.hover-dim': {
+          '@apply transition-all duration-150 hover:brightness-90': {},
+        },
+      });
+    }),
+  ],
 };
 
 export default config;
