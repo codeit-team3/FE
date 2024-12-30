f277075e9ed2075a0a966fb241700ac2d278d4b5
wynter24 <m.wynter24.k@gmail.com>
2024-12-30 10:34:41 +0900

    ♻️[Refactor] ClubCard의 onClick을 optional로 변경 #228

--- src/components/card/types/clubCard.ts
+++ src/components/card/types/clubCard.ts
@@ -14,7 +14,7 @@ interface ClubCard {
   clubStatus: 'pending' | 'confirmed' | 'closed'; // 개설 현황 TODO: 내가 만든 모임에서 '모임 완료' 상태 추가
 
   // 액션 (카드 클릭시 라우터 처리 등)
-  onClick: (clubId: number) => void;
+  onClick?: (clubId: number) => void;
 }
 
 interface DefaultClubCard extends ClubCard {
