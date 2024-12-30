ab7679b8b53d8373958fe62808df73f17e72408b
Minkyung Kim <97824352+wynter24@users.noreply.github.com>
2024-12-30 15:45:27 +0900

    ✨[Feat] 찜 목록 페이지 UI 생성 #157 (#235)
    * ♻️[Refactor] HeaderSection 공통 레이아웃 컴포넌트로 분리 #157
    
    * 🎨[Style] 불필요한 index 파일 제거 및 export 관리 단순화 #157
    
    * 🎨[Style] 변경된 파일 경로 수정 #157
    
    * ✨[Feat] 찜페이지 UI 생성 #157
    
    * ♻️[Refactor] FiterCheckbox 체크 아이콘 IcCheckOnly로 변경 #157
    
    * ♻️[Refactor] 공통 레이아웃 컴포넌트 HeaderSection 버튼 prop으로 변경 #157
    
    * 🐛[Fix] IcCheckOnly 속성을 camelCase로 수정 #157

--- src/components/common-layout/index.ts
+++ src/components/common-layout/index.ts
@@ -1,3 +1,5 @@
 export { default as FilterSection } from './FilterSection';
 export { default as SearchSection } from './SearchSection';
 export { default as CategoryTabs } from './CategoryTabs';
+export { default as FilterBar } from './FilterBar';
+export { default as HeaderSection } from './HeaderSection';
--- src/features/bookclub/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as BookClubMainPage } from './BookClubMainPage';
-export { default as HeaderSection } from './HeaderSection';
-export { default as ClubListSection } from './ClubListSection';
