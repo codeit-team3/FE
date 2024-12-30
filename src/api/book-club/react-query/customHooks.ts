ff6df92d081b419ad8afab17996dab4c4558ff85
wynter24 <m.wynter24.k@gmail.com>
2024-12-30 15:31:58 +0900

    ✨[Feat] 모임 참여 완료 api 연결 및 에러 처리 #228

--- src/api/book-club/react-query/customHooks.ts
+++ src/api/book-club/react-query/customHooks.ts
@@ -9,6 +9,7 @@ import {
   bookClubReviewAPI,
 } from '../index';
 import { WriteReviewParams } from '../types';
+import { AxiosError } from 'axios';
 
 export function useBookClubCreateMutation() {
   const queryClient = useQueryClient();
@@ -24,6 +25,19 @@ export function useBookClubCreateMutation() {
       });
       queryClient.invalidateQueries({
         queryKey: bookClubs.myJoined().queryKey,
+      });
+    },
+  });
+}
+
+export function useJoinBookClub() {
+  const queryClient = useQueryClient();
+
+  return useMutation<void, AxiosError<{ message: string }>, number>({
+    mutationFn: (id: number) => bookClubMemberAPI.join(id),
+    onSuccess: (_, id) => {
+      queryClient.invalidateQueries({
+        queryKey: bookClubs.detail(id).queryKey,
       });
     },
   });
