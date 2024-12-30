72e971ce9dedd709908944e22f5968f590040897
cloud0406 <cloud0406@naver.com>
2024-12-30 13:33:08 +0900

    ♻️[Refactor] 구조 리팩토링

--- src/components/search-box/SearchBox.tsx
+++ src/components/search-box/SearchBox.tsx
@@ -1,28 +1,38 @@
 import React, { ChangeEvent } from 'react';
 import SearchIcon from '../../../public/icons/SearchIcon';
+import { twMerge } from 'tailwind-merge';
 
 interface SearchBoxProps {
   value: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
+  icon?: React.ReactNode;
+  className?: string;
 }
 
 const SearchBox = ({
   value,
   onChange,
   placeholder = '검색어를 입력해주세요',
+  icon = <SearchIcon />,
+  className,
 }: SearchBoxProps) => {
   return (
-    <div className="flex items-center gap-1 rounded-lg border border-gray-200 px-3 py-2">
+    <div
+      className={twMerge(
+        'flex items-center gap-1 rounded-lg border border-gray-200 px-3 py-2',
+        className,
+      )}
+    >
       <div className="flex h-6 w-6 items-center justify-center text-gray-400">
-        <SearchIcon />
+        {icon}
       </div>
       <input
         type="text"
         value={value}
         onChange={onChange}
         placeholder={placeholder}
-        className="w-full text-sm text-gray-darker placeholder:text-gray-normal-03"
+        className="w-full bg-transparent text-sm text-gray-darker placeholder:text-gray-normal-03"
       />
     </div>
   );
