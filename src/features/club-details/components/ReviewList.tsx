48edbc456477f074e73ad2f7054ed446d8a54cae
wynter24 <m.wynter24.k@gmail.com>
2025-01-01 11:28:42 +0900

    💄[Design] 수정된 EmptyState 적용 #227

--- src/features/club-details/components/ReviewList.tsx
+++ src/features/club-details/components/ReviewList.tsx
@@ -4,6 +4,7 @@ import ClubReview from '@/components/written-review/club-review/ClubReview';
 import { ClubReviewResponse } from '../types';
 import DropDown from '@/components/drop-down/DropDown';
 import { DetailClubReviewParams } from '@/api/book-club/types';
+import EmptyState from '@/components/common-layout/EmptyState';
 
 function ReviewList({
   reviewInfo,
@@ -29,19 +30,26 @@ function ReviewList({
           onChangeSelection={(selectedLabel) => setSortingOrder(selectedLabel)}
         />
       </div>
-      <div className="flex flex-col gap-4 rounded-xl border-2 border-gray-normal-01 p-6">
-        {reviewInfo.reviews.map((review) => (
-          <ClubReview
-            key={review.id}
-            ratingCount={review.rating}
-            comment={review.content}
-            userProfile={{
-              profileImage: review.image || '/images/profile.png',
-              userName: review.nickname,
-              createdAt: review.createdAt,
-            }}
+      <div className="flex min-h-[554px] flex-col gap-4 rounded-xl border-2 border-gray-normal-01 p-6 md:min-h-[434px]">
+        {reviewInfo.reviews?.length > 0 ? (
+          reviewInfo.reviews.map((review) => (
+            <ClubReview
+              key={review.id}
+              ratingCount={review.rating}
+              comment={review.content}
+              userProfile={{
+                profileImage: review.image || '/images/profile.png',
+                userName: review.nickname,
+                createdAt: review.createdAt,
+              }}
+            />
+          ))
+        ) : (
+          <EmptyState
+            title="아직 등록된 리뷰가 없어요."
+            subtitle="지금 바로 모임에 참여하고 리뷰를 남겨보세요."
           />
-        ))}
+        )}
       </div>
     </section>
   );
