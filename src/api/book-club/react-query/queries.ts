ae2960c5b1d11c9c40db041f01c58d0e551356a3
cloud0406 <cloud0406@naver.com>
2025-01-03 19:04:00 +0900

    🐛[Fix] 쿼리키 무효화안되는거 수정

--- src/api/book-club/react-query/queries.ts
+++ src/api/book-club/react-query/queries.ts
@@ -6,7 +6,7 @@ import { bookClubMainAPI } from '@/api/book-club/bookClubMainAPI';
 
 export const bookClubs = createQueryKeys('bookClubs', {
   list: (filters?: BookClubParams) => ({
-    queryKey: [{ filters }],
+    queryKey: [{ filters: filters || {} }],
     queryFn: () => bookClubMainAPI.getBookClubs(filters),
   }),
 
@@ -15,12 +15,9 @@ export const bookClubs = createQueryKeys('bookClubs', {
     queryFn: () => bookClubMainAPI.getBookClubDetail(bookClubId),
     contextQueries: {
       reviews: (filters?: ClubDetailReviewFilters) => ({
-        queryKey: [{ filters }],
+        queryKey: [{ filters: filters || {} }],
         queryFn: () =>
-          bookClubReviewAPI.getReviews({
-            bookClubId,
-            params: filters,
-          }),
+          bookClubReviewAPI.getReviews({ bookClubId, params: filters }),
       }),
     },
   }),
@@ -30,15 +27,15 @@ export const bookClubs = createQueryKeys('bookClubs', {
     queryFn: () => ({}),
     contextQueries: {
       joined: (filters?: MyProfileParams) => ({
-        queryKey: [{ filters }],
+        queryKey: [{ filters: filters || {} }],
         queryFn: () => bookClubMainAPI.myJoined(filters),
       }),
       created: (filters?: MyProfileParams) => ({
-        queryKey: [{ filters }],
+        queryKey: [{ filters: filters || {} }],
         queryFn: () => bookClubMainAPI.myCreated(filters),
       }),
       reviews: (filters?: MyProfileParams) => ({
-        queryKey: [{ filters }],
+        queryKey: [{ filters: filters || {} }],
         queryFn: () => bookClubReviewAPI.myReviews(filters),
       }),
     },
@@ -49,15 +46,15 @@ export const bookClubs = createQueryKeys('bookClubs', {
     queryFn: () => ({}),
     contextQueries: {
       joined: (filters?: MyProfileParams) => ({
-        queryKey: [{ filters }],
+        queryKey: [{ filters: filters || {} }],
         queryFn: () => bookClubMainAPI.userJoined(userId, filters),
       }),
       created: (filters?: MyProfileParams) => ({
-        queryKey: [{ filters }],
+        queryKey: [{ filters: filters || {} }],
         queryFn: () => bookClubMainAPI.userCreated(userId, filters),
       }),
       reviews: (filters?: MyProfileParams) => ({
-        queryKey: [{ filters }],
+        queryKey: [{ filters: filters || {} }],
         queryFn: () =>
           bookClubReviewAPI.userReviews({ userId, params: filters }),
       }),
