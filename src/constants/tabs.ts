c9b39277ae0dac5e2ee20bededd585d9665a034e
sunnwave <ssun20886@gmail.com>
2025-01-03 09:16:09 +0900

    ♻️[Refactor] Tab 타입 리팩토링

--- src/constants/tabs.ts
+++ src/constants/tabs.ts
@@ -1,22 +1,42 @@
-export const BOOK_TABS = ['전체', '자유책', '지정책'] as const;
-
-export const CONTENT_TABS = ['모임', '교환'] as const;
-
-export enum CLUB_TABS {
-  MY_JOINED = '참여한 모임',
-  MY_CREATED = '만든 모임',
-  MY_REVIEW = '작성한 리뷰',
+// export const BOOK_TABS = ['전체', '자유책', '지정책'] as const;
+export enum BOOK_TABS {
+  ALL = '전체',
+  FREE = '자유책',
+  FIXED = '지정책',
 }
 
-export const EXCHANGE_TABS = [
-  '나의 교환',
-  '내가 등록한 책',
-  '교환 리뷰',
-] as const;
+// export const CONTENT_TABS = ['모임', '교환'] as const;
 
+export enum CONTENT_TABS {
+  CLUB = '모임',
+  EXCHANGE = '교환',
+}
+
+export enum CLUB_TABS {
+  JOINED = '참여한 모임',
+  CREATED = '만든 모임',
+  REVIEW = '작성한 리뷰',
+}
+
+export enum EXCHANGE_TABS {
+  EXCHANGED = '교환 내역',
+  UPLOADED = '등록한 책',
+  REVIEW = '교환 리뷰',
+}
+
+type ValueOf<T> = T[keyof T];
 export type TabType = 'MAIN_TAB' | 'SUB_TAB';
-export type clubTabType = 'MY_JOINED' | 'MY_CREATED' | 'MY_REVIEW';
-export type BookTab = (typeof BOOK_TABS)[number];
-export type ContentTab = (typeof CONTENT_TABS)[number];
-export type ClubTab = (typeof CLUB_TABS)[clubTabType];
-export type ExchangeTab = (typeof EXCHANGE_TABS)[number];
+
+// export type clubTabType = 'JOINED' | 'CREATED' | 'REVIEW';
+// export type exchangeTabType = 'EXCHANGED' | 'UPLOADED' | 'REVIEW';
+
+// export type bookTab = (typeof BOOK_TABS)[number];
+// export type contentTab = (typeof CONTENT_TABS)[number];
+
+// export type clubTab = (typeof CLUB_TABS)[clubTabType];
+// export type exchangeTab = (typeof EXCHANGE_TABS)[exchangeTabType];
+
+export type bookTab = ValueOf<typeof BOOK_TABS>;
+export type contentTab = ValueOf<typeof CONTENT_TABS>;
+export type clubTab = ValueOf<typeof CLUB_TABS>;
+export type exchangeTab = ValueOf<typeof EXCHANGE_TABS>;
--- src/features/profile/components/exchange/ExchangeContents.tsx
+++ src/features/profile/components/exchange/ExchangeContents.tsx
@@ -1,22 +1,24 @@
 import SortingButton from '@/components/sorting-button/SortingButton';
-import { EXCHANGE_TABS, ExchangeTab } from '@/constants';
+import { EXCHANGE_TABS, exchangeTab } from '@/constants';
 import { useState } from 'react';
 import { MyRegisteredBooks, TradeRecords, TradeReviews } from '../exchange';
 import Tab from '@/components/tab/Tab';
+import { orderType } from '@/types/bookclubs';
 
 export default function ExchangeContents() {
-  const [sortBy, setSortBy] = useState<string | undefined>('NEWEST');
-  const [selectedList, setSelectedList] = useState<ExchangeTab>(
-    EXCHANGE_TABS[0],
+  const [order, setOrder] = useState<orderType>('DESC');
+  const [selectedList, setSelectedList] = useState<exchangeTab>(
+    EXCHANGE_TABS.EXCHANGED,
   );
 
-  const renderList = (selectedList: ExchangeTab) => {
+  console.log(order);
+  const renderList = (selectedList: exchangeTab) => {
     switch (selectedList) {
-      case EXCHANGE_TABS[0]:
+      case EXCHANGE_TABS.EXCHANGED:
         return <TradeRecords />;
-      case EXCHANGE_TABS[1]:
+      case EXCHANGE_TABS.UPLOADED:
         return <MyRegisteredBooks />;
-      case EXCHANGE_TABS[2]:
+      case EXCHANGE_TABS.REVIEW:
         return <TradeReviews />;
     }
   };
@@ -28,7 +30,7 @@ export default function ExchangeContents() {
     >
       <div className="flex items-center justify-between gap-x-2 sm:overflow-auto sm:whitespace-nowrap sm:[&::-webkit-scrollbar]:hidden">
         <Tab
-          items={EXCHANGE_TABS}
+          items={Object.values(EXCHANGE_TABS)}
           activeTab={selectedList}
           onTabChange={(item) => setSelectedList(item)}
           tabType="SUB_TAB"
@@ -37,8 +39,7 @@ export default function ExchangeContents() {
         <SortingButton
           variant="byDate"
           onClickSorting={(order) => {
-            setSortBy(order);
-            console.log(sortBy);
+            setOrder(order);
           }}
         />
       </div>
