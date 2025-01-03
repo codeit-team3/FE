35bc223915620e90d3c9a9b8e295394f0f56e13e
wynter24 <m.wynter24.k@gmail.com>
2025-01-03 19:15:13 +0900

    ✨[Feat] 평점별 리뷰 개개수 표시 #270

--- src/features/club-details/components/ReviewSummary.tsx
+++ src/features/club-details/components/ReviewSummary.tsx
@@ -3,6 +3,20 @@ import RatingDisplay from '@/components/rating-display/RatingDisplay';
 import { ClubReviewResponse } from '../types';
 
 function ReviewSummary({ reviewInfo }: { reviewInfo: ClubReviewResponse }) {
+  const ratingCounts = (score: number) => {
+    return reviewInfo.ratingCounts[
+      score === 5
+        ? 'five'
+        : score === 4
+          ? 'four'
+          : score === 3
+            ? 'three'
+            : score === 2
+              ? 'two'
+              : 'one'
+    ];
+  };
+
   return (
     <section>
       <h2 className="mb-[10px] text-[20px] font-semibold text-gray-black">
@@ -22,13 +36,13 @@ function ReviewSummary({ reviewInfo }: { reviewInfo: ClubReviewResponse }) {
           <RatingDisplay ratingCount={reviewInfo.averageScore} />
         </div>
         <div className="flex flex-col gap-1">
-          {Array.from({ length: 5 }).map((_, index) => (
+          {[5, 4, 3, 2, 1].map((score) => (
             <div
               className="flex items-center gap-3 text-sm font-medium"
-              key={`rate-${5 - index}`}
+              key={`rate-${score}`}
             >
               <span className="whitespace-nowrap text-gray-dark-03">
-                {5 - index}점
+                {score}점
               </span>
               <div
                 className="flex-1"
@@ -36,9 +50,14 @@ function ReviewSummary({ reviewInfo }: { reviewInfo: ClubReviewResponse }) {
                   minWidth: 'clamp(62px, 20vw, 240px)',
                 }}
               >
-                <ProgressBar percentage={5} color="bg-gray-dark-02" />
+                <ProgressBar
+                  percentage={ratingCounts(score)}
+                  color="bg-gray-dark-02"
+                />
               </div>
-              <span className="ml-auto text-gray-dark-01">2</span>
+              <span className="ml-auto text-gray-dark-01">
+                {ratingCounts(score)}
+              </span>
             </div>
           ))}
         </div>
