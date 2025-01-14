8522c3060218376b08585b6826087a50268f84ac
wynter24 <m.wynter24.k@gmail.com>
2025-01-14 15:02:48 +0900

    ✨[Feat] ssr로 fetching 한 데이터를 useQuery 초기데이터로 설정 #308

--- src/features/bookclub/hooks/useFetchBookClubList.ts
+++ src/features/bookclub/hooks/useFetchBookClubList.ts
@@ -1,17 +1,21 @@
 import { useState } from 'react';
-import { BookClubParams } from '@/types/bookclubs';
+import { BookClub, BookClubParams } from '@/types/bookclubs';
 import { useQuery } from '@tanstack/react-query';
 import { bookClubs } from '@/api/book-club/react-query';
 import { DEFAULT_FILTERS } from '@/lib/constants/filters';
 
-const useBookClubList = () => {
+const useBookClubList = ({ initialData }: { initialData: BookClub[] }) => {
+  // const [filters, setFilters] = useState<BookClubParams>(defaultFilters);
   const [filters, setFilters] = useState<BookClubParams>(DEFAULT_FILTERS);
 
   const { data, isLoading, error } = useQuery({
     ...bookClubs.list(filters),
+    initialData: { bookClubs: initialData }, // SSR로 전달받은 초기 데이터 설정
+    staleTime: 1000 * 60 * 5, // 5분 동안 데이터를 Fresh 상태로 유지
   });
 
   const clubList = data?.bookClubs;
+  console.log(clubList);
 
   const updateFilters = (newFilters: Partial<BookClubParams>) => {
     setFilters((prevFilters) => ({ ...prevFilters, ...newFilters }));
