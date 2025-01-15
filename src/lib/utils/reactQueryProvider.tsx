036f7e85447b3b756f836a8ca42fc8f854a14de5
cloud0406 <cloud0406@naver.com>
2025-01-15 15:20:43 +0900

    ♻️[Refactor] 에러 바운더리 전파를 위해 retryOnMount 옵션 제거

--- src/lib/utils/reactQueryProvider.tsx
+++ src/lib/utils/reactQueryProvider.tsx
@@ -9,7 +9,7 @@ export const queryClient = new QueryClient({
       refetchOnMount: true, // 컴포넌트가 마운트될 때 데이터를 다시 가져올지 여부
       retry: 0, // 실패한 쿼리 재시도 횟수
       refetchOnReconnect: false, // 네트워크 재연결시 데이터를 다시 가져올지 여부
-      retryOnMount: false, // 마운트 시 실패한 쿼리 재시도 여부
+      // retryOnMount: false, // 마운트 시 실패한 쿼리 재시도 여부
       staleTime: 1000 * 60 * 5, // 데이터가 'fresh'한 상태로 유지되는 시간 (5분)
       gcTime: 1000 * 60 * 10, // 사용하지 않는 캐시 데이터가 메모리에서 제거되기까지의 시간 (10분)
       throwOnError: true,
