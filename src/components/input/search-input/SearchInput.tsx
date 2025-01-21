b56e76bdc67b6a526ab1305c53083bf916dab457
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2025-01-21 14:24:03 +0900

    feature: 모임 검색 debounce 적용 #320

--- src/components/input/search-input/SearchInput.tsx
+++ src/components/input/search-input/SearchInput.tsx
@@ -1,6 +1,7 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useMemo, useState } from 'react';
 import Input from '../Input';
 import SearchIcon from '../../../../public/icons/SearchIcon';
+import { debounce } from 'lodash';
 
 interface SearchInputProps {
   value: string;
@@ -8,11 +9,27 @@ interface SearchInputProps {
 }
 
 function SearchInput({ value, onChange }: SearchInputProps) {
+  const [inputValue, setInputValue] = useState(value);
+
+  const debouncedOnChange = useMemo(
+    () =>
+      debounce((value: string) => {
+        onChange({ target: { value } } as ChangeEvent<HTMLInputElement>);
+      }, 300),
+    [onChange],
+  );
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setInputValue(newValue);
+    debouncedOnChange(newValue);
+  };
+
   return (
     <div className="bg-white">
       <Input
-        value={value}
-        onChange={onChange}
+        value={inputValue}
+        onChange={handleChange}
         placeholder={'검색어를 입력해주세요'}
         icon={<SearchIcon />}
       />
