72f52f7d95c8e6c29a0ae3fe4b6024e5e2076d33
wynter24 <m.wynter24.k@gmail.com>
2025-01-24 14:36:15 +0900

    📦[Chore] next.config env 설정 #308

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,9 @@ const nextConfig: NextConfig = {
   instrumentation: {
     enabled: true,
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
