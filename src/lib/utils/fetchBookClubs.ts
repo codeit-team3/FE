24cf4e7be823eebfecad0c236d7c1f99b9e810c3
wynter24 <m.wynter24.k@gmail.com>
2025-01-24 11:30:47 +0900

    ✅[Test] 환경 변수 에러 확인 #308

--- src/lib/utils/fetchBookClubs.ts
+++ src/lib/utils/fetchBookClubs.ts
@@ -1,12 +1,15 @@
 export async function fetchBookClubs() {
-  const baseUrl = process.env.API_URL || process.env.NEXT_PUBLIC_API_URL;
+  // const baseUrl = process.env.API_URL || process.env.NEXT_PUBLIC_API_URL;
   try {
-    const res = await fetch(`${baseUrl}/book-clubs?size=100`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
+    const res = await fetch(
+      `https://d3eoy4ym225l85.cloudfront.net/api/v1/book-clubs?size=100`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
       },
-    });
+    );
 
     if (!res.ok) {
       throw new Error(`HTTP error! status: ${res.status}`);
