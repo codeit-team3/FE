8300169a0b8e948f00d298df97ffb4c43b7d55db
Jeongho Kim <cloud0406@naver.com>
2025-02-06 11:32:11 +0900

    ✨ [Feature] 에러 전역 처리 (#321)
    * ✨[Feat] 에러 바운더리 및 핸들링 wrapper 적용
    
    * ♻️[Refactor] 유저 정보 훅 enabled 추가
    
    * ✨[Feat] 내가 참여한 모임 에러바운더리 적용
    
    * ♻️[Refactor] 에러 바운더리 전파를 위해 retryOnMount 옵션 제거
    
    * ✨[Feat] error.tsx 템플릿 구현
    
    * ✨[Feat] 북클럽 생성 페이지 Error.tsx 적용
    
    * ✨[Feat] not found 적용
    
    * ✨[Feat] 글로벌 에러 적용
    
    * ✨[Feat] 전역 토스트 설정
    
    * ✨[Feat] global callback 적용
    
    * 🚚[Rename] 에러 파일들 컴포넌트로 분리
    
    * ✨[Feat] alertIcon 추가
    
    * 💄[Design] 에러 폴백 컴포넌트 디자인 수정
    
    * 💄[Design] 서버사이드 에러 템플릿 디자인 변경
    
    * 💄[Design] notfound, global 에러 페이지 이미지 추가
    
    * 📝[Docs] Info 테스트 코드 임시 주석 처리

--- src/features/profile/container/ClubContents.tsx
+++ src/features/profile/container/ClubContents.tsx
@@ -13,6 +13,9 @@ import {
   MyWrittenReviewList,
   WrittenReviewList,
 } from '../container/index';
+import ErrorHandlingWrapper from '@/components/error/ErrorHandlingWrapper';
+import ErrorFallback from '@/components/error/ErrorFallback';
+import Loading from '@/components/loading/Loading';
 
 export default function ClubContents({ isMyPage }: ProfilePageProps) {
   const [order, setOrder] = useState<orderType>('DESC');
@@ -62,7 +65,14 @@ export default function ClubContents({ isMyPage }: ProfilePageProps) {
         />
       </div>
 
-      <div className="pt-[18px]">{renderList(selectedList)}</div>
+      <div className="pt-[18px]">
+        <ErrorHandlingWrapper
+          fallbackComponent={ErrorFallback}
+          suspenseFallback={<Loading />}
+        >
+          {renderList(selectedList)}
+        </ErrorHandlingWrapper>
+      </div>
     </div>
   );
 }
